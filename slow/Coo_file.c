a1dc68b2515216879821687d0f6529f7e924d611
usoskin <usoskin>
1998-05-20 17:37:59 +0000

    *** empty log message ***

---
+++ slow/Coo_file.c
@@ -0,0 +1,300 @@
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+
+#include <ams/types.h>
+#include <ams/castcp.h>
+
+#include "Coordinates.h" // cooradinate recalculation 
+#include "file_reading.h" // reading CAS files
+
+
+#define foot_cm 30.48
+#define debug 0
+
+CASblock  buffer;
+#if 0
+
+typedef struct  {
+  int   size;                        /* Actual size in little endian         */
+  int   pktNum;                      /* Packet number for first NASCOM block */
+  int   gsedTime[CAS_TAGS_INDEX+1];  /* gsed time for each NASCOM block      */
+  int   nasTime[CAS_TAGS_INDEX+1];   /* NASCOM time for each NASCOM block    */
+  byte  CASmsg[16*600];              /* Unmodified CAS message.              */
+                                     /* (Can't be bigger than this.)         */
+} CASblock;
+
+#endif
+int day, current_block=0;
+
+
+double CASvalue (int offset, int length) {
+  byte val[8];
+  double dval=0;
+  int i;
+
+  for (i=0; i<length; i++) {
+    val[i]=buffer.CASmsg[offset+length-i-4];
+    if (debug)
+      printf("value = %02x ",val[i]);
+  }
+  if (debug)
+    printf("\n");
+  dval=(double)*((double*)val);
+  return dval;
+}
+
+double CAStime(int offset) { /* based on H.Suter's function */
+  ulong musec, sec, min, hours, days, time;
+  double times;
+  int i;
+  union {
+    ulong val;
+    struct {
+      uint msec : 13;		// microseconds
+      uint nil  :  5;		// void
+      uint usec :  4;		// units of seconds
+      uint tsec :  3;		// tens of seconds
+      uint umin :  4;		// units of minutes
+      uint tmin :  3;		// tens of minute
+      uint uhour:  4;		// units of hours
+      uint thour:  2;		// tens of hours
+      uint uday :  4;		// units of days
+      uint tday :  4;		// tens of days
+      uint hday :  2;		// hundreds of days
+    } CASdate;
+  } t;
+
+  if (buffer.CASmsg[offset-4] == 0) {
+    time = buffer.CASmsg[offset-3];
+    for (i=1; i<6; i++) 
+      time = time << 8 | buffer.CASmsg[offset-3+i];
+    t.val=time;
+    musec = t.CASdate.msec / 8;
+    sec   = t.CASdate.usec  + 10*t.CASdate.tsec;
+    min   = t.CASdate.umin  + 10*t.CASdate.tmin;
+    hours = t.CASdate.uhour + 10*t.CASdate.thour;
+    days  = t.CASdate.uday  + 10*t.CASdate.tday +  100*t.CASdate.hday;
+
+    times = (day*86400 + hours*3600 + min*60 + sec) + musec/1000.;
+
+    return times;
+  }
+  else 
+    return 0;
+}
+
+float double_float(double *a) {
+  float b;
+  if ((*a == 0) || (abs(*a)>3.4e+30) || (abs(*a)<3.4e-30))
+    return 0.;
+  b=(float) (*a);
+  return b;
+}
+
+int main(int argc, char *argv[]) {
+
+double Coo_M1950[3], Vel_M1950[3], q[4], Greenw_Phi, Vel_angle;
+polar Geo,Solar;
+Euler Euler_LVLH;
+FILE *fp, *ftemp, *fout, *fl;
+int i,k, file_nu, ii=30;
+uint Data[Record_nbytes/4], *pData;
+uint crc;
+time_t cur_time;
+float fbuf;
+int block_size;
+char current_f_name[80], chb80[80];
+
+
+utime = 894358568;
+file_n=0;
+record_n=Umax=0;
+Umin = 0xffffffff;
+
+  CAS_dir=getenv("CASDataDir");
+  Coo_db_dir=getenv("CooDbDir");
+
+  while(1) {  /*GLOBAL LOOP */
+
+NEW_FILES: /* look for new files in the directory */
+    new_files=0;
+    new_files=new_files_coming();
+    if (!new_files) {
+      ii++;
+      if (ii>30) {
+	time(&cur_time);
+	printf("no new files. waiting......%s\n",ctime(&cur_time));
+	ii=0;
+      }
+      sleep(60);
+      goto NEW_FILES;
+    }
+    
+    for (file_nu=0; file_nu<= new_file_c; file_nu++) { /* Main Loop */
+      fp = open_file(file_nu);
+      if (fp == NULL)
+	goto END_LOOP;
+      
+  BEG: ;
+      if (feof(fp)) {
+	fclose(fp);
+	goto END_LOOP;
+      }
+    
+    /*reading data */
+      fread(&block_size,sizeof(int),1,fp);
+      for (i=0; i<20; i++) 
+	fread(&k, sizeof(int),1,fp);
+      fread(&buffer.CASmsg,1,(block_size-20*4),fp);
+
+
+      if (record_n>=Record_in_file) {
+	fclose(ftemp);
+	ftemp=fopen(temp_file,"r");
+	fread(&Data,Record_nbytes,1,ftemp);
+	fclose(ftemp);
+	pData = &Data[0];
+	crc = _CalcCRC(pData);
+	time(&cur_time);
+	sprintf(current_f_name,"%s/ShuttlePar.1.%d",Coo_db_dir,Umin);
+	fout=fopen(current_f_name,"w");
+	fwrite(&Data,1,Record_nbytes,fout);
+	fwrite(&crc,sizeof(time_t),1,fout);
+	fwrite(&cur_time,sizeof(time_t),1,fout);
+	fwrite(&Umin,sizeof(time_t),1,fout);
+	fwrite(&Umax,sizeof(time_t),1,fout);
+	fclose(fout);
+	sprintf(chb80,"rm %s",temp_file);
+	system(chb80);
+	printf("======================%d\n",file_n);
+	file_n++;
+	record_n=Umax=0;
+	Umin = 0xffffffff;
+	if (file_n > 90) 
+	  exit (1);
+	//	  flog = fopen("temp.log","a");
+	  fclose(fout);
+      }
+
+	/* Reading M1950 coordinates and velocities from CAs */
+      Coo_M1950[0]=CASvalue((4490),8) * foot_cm;
+      Coo_M1950[1]=CASvalue((4508),8) * foot_cm;
+      Coo_M1950[2]=CASvalue((4526),8) * foot_cm;
+
+      Vel_M1950[0]=CASvalue((4678),8) * foot_cm;
+      Vel_M1950[1]=CASvalue((4701),8) * foot_cm;
+      Vel_M1950[2]=CASvalue((4715),8) * foot_cm;
+//        printf(".......................... %d %d\n",record_n,Umin); 
+
+	/* Reading quaterions from CAS */
+      q[0]=CASvalue((3291),8);
+      q[1]=CASvalue((3305),8);
+      q[2]=CASvalue((3319),8);
+      q[3]=CASvalue((3333),8);
+
+	/*	times = CAStime(10);   time, millisec, from the New Year 
+	utime = (time_t) (times) + Year1998;*/
+
+      utime ++; 
+      
+      if ((Radius(Coo_M1950)<=.1) || (Radius(Vel_M1950)<=.1))
+	goto BEG;
+
+      Coordinates(Coo_M1950,Vel_M1950,q,utime,&Geo,&Vel_angle,&Euler_LVLH,&Solar,&Greenw_Phi);
+
+      if ((Geo.R<1) || (Geo.R>1.e+9) || (Geo.Teta>(Pi/2)))
+	printf("\a\aGeo %e  %e  %e\n",Geo.R,Geo.Teta,Geo.Phi);
+      if (Vel_angle<0.0005)
+	printf("\a\aVel_angle %e\n",Vel_angle);
+
+//	Vel_angle = Radius(Vel_M1950)/Radius(Coo_M1950);
+
+      if (record_n == 0)
+	ftemp=fopen(temp_file,"w");
+
+      fbuf=(float)(Geo.R);
+      fwrite(&fbuf,sizeof(float),1,ftemp);
+      fbuf=(float)(Geo.Teta);
+      fwrite(&fbuf,sizeof(float),1,ftemp);
+      fbuf=(float)(Geo.Phi);
+      fwrite(&fbuf,sizeof(float),1,ftemp);
+      fbuf=(float)(Greenw_Phi);
+      fwrite(&fbuf,sizeof(float),1,ftemp);
+      fbuf=(float)(Euler_LVLH.Yaw);
+      fwrite(&fbuf,sizeof(float),1,ftemp);
+      fbuf=(float)(Euler_LVLH.Pitch);
+      fwrite(&fbuf,sizeof(float),1,ftemp);
+      fbuf=(float)(Euler_LVLH.Roll);
+      fwrite(&fbuf,sizeof(float),1,ftemp);
+      fbuf=(float)(Vel_angle);
+      fwrite(&fbuf,sizeof(float),1,ftemp);
+      fbuf=(float)(Solar.R);
+      fwrite(&fbuf,sizeof(float),1,ftemp);
+      fbuf=(float)(Solar.Teta);
+      fwrite(&fbuf,sizeof(float),1,ftemp);
+      fbuf=(float)(Solar.Phi);
+      fwrite(&fbuf,sizeof(float),1,ftemp);
+      fwrite(&utime,sizeof(time_t),1,ftemp);
+      if (utime<Umin) Umin=utime;
+      if (utime>=Umax) Umax=utime+1;
+      record_n++;
+
+      memset( (char *) &buffer, 0, sizeof(buffer));
+      goto BEG;
+
+    END_LOOP:;
+      fl=fopen(last_name,"w");
+      fprintf(fl,"%s\n",new_file_n[new_file_c]);
+      fprintf(fl,"%d\n",new_file_t[new_file_c]);
+      fclose(fl);
+    }
+    if (record_n>0) { 
+      for (i=record_n; i<Record_in_file; i++) {
+	fbuf=(float)(Geo.R);
+	fwrite(&fbuf,sizeof(float),1,ftemp);
+	fbuf=(float)(Geo.Teta);
+	fwrite(&fbuf,sizeof(float),1,ftemp);
+	fbuf=(float)(Geo.Phi);
+	fwrite(&fbuf,sizeof(float),1,ftemp);
+	fbuf=(float)(Greenw_Phi);
+	fwrite(&fbuf,sizeof(float),1,ftemp);
+	fbuf=(float)(Euler_LVLH.Yaw);
+	fwrite(&fbuf,sizeof(float),1,ftemp);
+	fbuf=(float)(Euler_LVLH.Pitch);
+	fwrite(&fbuf,sizeof(float),1,ftemp);
+	fbuf=(float)(Euler_LVLH.Roll);
+	fwrite(&fbuf,sizeof(float),1,ftemp);
+	fbuf=(float)(Vel_angle);
+	fwrite(&fbuf,sizeof(float),1,ftemp);
+	fbuf=(float)(Solar.R);
+	fwrite(&fbuf,sizeof(float),1,ftemp);
+	fbuf=(float)(Solar.Teta);
+	fwrite(&fbuf,sizeof(float),1,ftemp);
+	fbuf=(float)(Solar.Phi);
+	fwrite(&fbuf,sizeof(float),1,ftemp);
+	fwrite(&utime,sizeof(time_t),1,ftemp);
+      }
+      fclose(ftemp);
+      ftemp=fopen(temp_file,"r");
+      fread(&Data,Record_nbytes,1,ftemp);
+      fclose(ftemp);
+      pData = &Data[0];
+      crc = _CalcCRC(pData);
+      time(&cur_time);
+      sprintf(current_f_name,"%s/ShuttlePar.1.%d",Coo_db_dir,cur_time);
+      fout=fopen(current_f_name,"w");
+      fwrite(&Data,1,Record_nbytes,fout);
+      fwrite(&crc,sizeof(time_t),1,fout);
+      fwrite(&cur_time,sizeof(time_t),1,fout);
+      fwrite(&Umin,sizeof(time_t),1,fout);
+      fwrite(&Umax,sizeof(time_t),1,fout);
+      fclose(fout);    
+    }
+    goto NEW_FILES;
+  }
+  return(0);
+}
---
+++ slow/file_reading.h
@@ -0,0 +1,82 @@
+#include <sys/types.h>
+#include <dirent.h>
+#include <fnmatch.h>
+
+#define buf_n 200  /* number of file names buffered */
+
+typedef char ch80[80];
+
+int last_file_read;
+char * CAS_dir, * Coo_db_dir, last_file_n[80], chb80[80];
+char last_name[80]="last.file";
+int new_files=0;
+time_t file_t, last_file_time;
+
+ch80    new_file_n[buf_n];
+time_t  new_file_t[buf_n];
+int new_file_c; /* how many new files */
+
+/* opens a CAS file */
+int new_files_coming() {
+  FILE *last_f;
+  char chbuf80[80], last_file_n[80];
+  time_t last_file_time;
+  DIR *dirp;
+  struct dirent *dp;
+  struct stat statf;
+  time_t tmin=0x7ffffffa;
+
+
+  /* reads the last read file name */
+  last_f=fopen(last_name,"r");
+  if (last_f == NULL) {
+    last_file_time = 0;
+  }
+  else {
+    fgets(chb80,80,last_f);
+    if (&chb80 == NULL) {
+      printf("ERROR:Coo_file: file %s corrupted. Stop\n",last_name);
+      exit(0);
+    }
+    sprintf(last_file_n,"%s",chb80);
+    fgets(chb80,80,last_f);
+    last_file_time=atol(chb80);
+    fclose(last_f);
+  }
+  /* list directory */
+  if ((dirp = opendir(CAS_dir)) == NULL) {
+    printf( "Coo_file: open (%s) fails. Stop. \nCheck CASDir set \n", CAS_dir);
+    exit(0);
+  }
+  new_file_c=-1;
+
+  while ((dp = readdir( dirp)) != NULL) {
+    if (fnmatch( "[0-9][0-9][0-9][0-9][0-9][0-9]", dp->d_name, 0) == 0) {
+      sprintf(chbuf80,"%s/%s",CAS_dir,dp->d_name);
+      stat(chbuf80,&statf);
+      file_t = statf.st_mtime;
+      if ((file_t>last_file_time) && (file_t<tmin)) {
+	new_file_c++;
+	new_file_t[new_file_c]=file_t;
+	sprintf(new_file_n[new_file_c],"%s",dp->d_name);
+	new_files=1;
+	if (new_file_c >= (buf_n-1))
+	  return(new_files);
+      }
+    }
+  }
+  return(new_files);
+}
+
+FILE * open_file(int i) {
+char chbuf[80];
+FILE *fp;
+
+  sprintf(chbuf,"%s/%s",CAS_dir,new_file_n[i]);
+  fp=fopen(chbuf,"r");
+  if (fp == NULL) {
+    printf("Warning: CAS file %s cannot be open\n",new_file_n[i]);
+    return(NULL);
+  }
+  return(fp);
+}
