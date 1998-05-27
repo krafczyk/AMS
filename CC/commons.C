29d7ac127d91a612e98121ca1aae7f8a72295bca
vitali <vitali>
1998-05-27 13:12:15 +0000

    +

--- CC/commons.C
+++ CC/commons.C
@@ -53,7 +53,7 @@ GCKINE_DEF GCKINE;
 // AMSDATADIR_DEF AMSDATADIR
 
 char AMSCommonsI::_version[]="v2.16";
-integer AMSCommonsI::_build=9;
+integer AMSCommonsI::_build=11;
 
 AMSCommonsI::AMSCommonsI(){
   init();
