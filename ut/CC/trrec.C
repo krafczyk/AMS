e930049938f4e8f3af7d4e027c7448039ecbfd1b
choutko <choutko>
1998-12-18 10:32:46 +0000

    +

--- ut/CC/trrec.C
+++ ut/CC/trrec.C
@@ -2883,7 +2883,7 @@ void AMSTrTrack::_crHit(){
  if(AMSTrAligFit::glDBOK(_Address))setstatus(AMSDBc::GlobalDB);
 // if(!TRALIG.UpdateDB &&  !checkstatus(AMSDBc::GlobalDB))par=AMSTrAligPar::SearchDB(_Address, found,_Dbase);
  if(!TRALIG.UpdateDB )par=AMSTrAligPar::SearchDB(_Address, found,_Dbase);
-  if(found){
+  if(found && fabs(_Dbase[1]-1)>TRALIG.GlobalGoodLimit){
    for(int i=0;i<_NHits;i++){
     int plane=patconf[_Pattern][i]-1;
     for(int j=0;j<3;j++){
