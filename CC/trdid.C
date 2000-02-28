454bc9233b9d937d4594665e97c5ca327d9b8b19
choutko <choutko>
2000-02-28 18:11:46 +0000

    *** empty log message ***

---
+++ CC/trdid.C
@@ -0,0 +1,42 @@
+// Author V. Choutko 24-may-1996
+ 
+#include <assert.h>
+#include <trdid.h>
+#include <cern.h>
+#include <commons.h>
+#include <amsstl.h>
+#include <stdio.h>
+using namespace trdconst;
+
+uinteger AMSTRDIdGeom::cmpt() const{
+return _octagon+mtrdo*_layer+mtrdo*maxlay*_ladder+mtrdo*maxlay*maxlad*_tube+1;
+}
+AMSID AMSTRDIdGeom::crgid() const{
+         static char name[5]="TRDT";
+         return AMSID(name,cmpt());
+}
+void AMSTRDIdGeom::_check(){
+  if(_octagon>0)
+  cerr <<"AMSTRDIdGeom-ctor-octagon out of bounds "<<_octagon<<endl; 
+  else if( _layer>TRDDBc::LayersNo(_octagon)-1)
+  cerr <<"AMSTRDIdGeom-ctor-layer out of bounds "<<_layer<<endl; 
+  else if(_ladder>TRDDBc::LaddersNo(_octagon,_layer)-1)
+  cerr <<"AMSTRDIdGeom-ctor-ladder out of bounds "<<_ladder<<endl; 
+  else if(_tube>TRDDBc::TubesNo(_octagon,_layer,_ladder)-1)
+  cerr <<"AMSTRDIdGeom-ctor-tube out of bounds "<<_tube<<endl; 
+}
+
+integer AMSTRDIdGeom::operator == (const AMSTRDIdGeom &o){
+  return (_layer==o._layer && _ladder==o._ladder && _tube==o._tube && _octagon==o._octagon);
+}
+
+AMSTRDIdGeom::AMSTRDIdGeom(integer idsoft){
+_octagon=(idsoft-1)%mtrdo;
+_layer=((idsoft-1)/mtrdo)%maxlay;
+_ladder=((idsoft-1)/mtrdo/maxlay)%maxlad;
+_tube=((idsoft-1)/mtrdo/maxlay/maxlad)%maxtube;
+
+#ifdef __AMSDEBUG__
+_check();
+#endif
+}
---
+++ include/trdid.h
@@ -0,0 +1,45 @@
+#ifndef __AMSTRDID__
+#define __AMSTRDID__
+#include <typedefs.h>
+#include <amsdbc.h>
+#include <iostream.h>
+#include <amsstl.h>
+#include <commons.h>
+#include <link.h>
+#include <job.h>
+#include <trddbc.h>
+using trdconst::maxo;
+using trdconst::mtrdo;
+using trdconst::maxtube;
+
+namespace trdid{
+
+}
+class AMSTRDIdGeom{
+uinteger _octagon;  // from 0 to 0l
+uinteger _layer;    // from 0 to TRDDBc::LayersNo(_octagon)-1
+uinteger _ladder;   // from 0 to TRDDBc::LaddersNo(_octagon,_layer)-1
+uinteger _tube;   // from 0 to TRDDBc::TubesNo(_octagon,_layer,_ladder)-1
+void _check();
+public:
+friend ostream &operator << (ostream &o, const  AMSTRDIdGeom &b )
+   {return o<<" lay "<<b._layer<<" lad "<<b._ladder<<" tube "<<b._tube<<endl;}
+AMSTRDIdGeom():_layer(0),_ladder(0),_tube(0),_octagon(0){};
+~AMSTRDIdGeom(){};
+AMSTRDIdGeom(integer layer,integer ladder,integer tube, integer octagon=0):
+_layer(layer),_ladder(ladder),_tube(tube),_octagon(octagon){
+#ifdef __AMSDEBUG__
+_check();
+#endif
+}
+
+AMSTRDIdGeom(integer idsoft);    // from geant to class
+AMSID crgid() const;   // based on cmpt
+integer operator == (const AMSTRDIdGeom &o);
+inline uinteger getlayer() const {return _layer;}
+inline uinteger getladder()const {return _ladder;}
+inline uinteger gettube()const {return _tube;}
+uinteger cmpt() const;
+};
+#endif
+
