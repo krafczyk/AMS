5da5dcfc78597200b8349a9a469d9c713955ebef
choutko <choutko>
2000-07-13 10:31:50 +0000

    *** empty log message ***

--- ut/CC/commons.C
+++ ut/CC/commons.C
@@ -76,7 +76,7 @@ GCKINE_DEF GCKINE;
 // AMSDATADIR_DEF AMSDATADIR
 
 char AMSCommonsI::_version[]="v3.00";
-integer AMSCommonsI::_build=937;
+integer AMSCommonsI::_build=938;
 AMSCommonsI::AMSCommonsI(){
   init();
 }
