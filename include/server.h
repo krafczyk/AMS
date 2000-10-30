1a1dbdfdcdd5901edac6ae3909f6e2a4e91375af
choutko <choutko>
2000-10-30 10:09:31 +0000

    *** empty log message ***

--- include/server.h
+++ include/server.h
@@ -6,36 +6,85 @@
 #include <unistd.h>
 #include <list>
 #include <queue>
+#include <server-cpp-common.hh> 
 #include <server-cpp-skels.hh>
 #include <ORBitservices/CosNaming.hh>
+#include <node.h>
 
-
-class Producer_impl : public virtual POA_DPS::Producer{
-public:
-  virtual void sendId(const DPS::Client::CID& cid, int p, int e) throw (CORBA::SystemException);
-  virtual void getACL(ACL_out acl);
-  virtual void getNCL(NCL_out ncl);
-  virtual TDV* getTDV(const char * name, DPS::time i, DPS::time b, DPS::time e);
-  void sendTDV( const TDV & tdv, const char * name, DPS::time Insert, DPS::time Begin, DPS::time End, int Propagate, int  Error );
-  void sendCurrentInfo(const  CurrentInfo &ci, int  Propagate, int Error);
-  TDVTable * getTDVTable(const char * Name, const  RunEv& rv,int  Propagate, int Error);
-  void sendNtupleEnd(const  NtupleEnd & ne, int Propagate, int Error);
-  void sendEventStatusEnd(const  EventStatusEnd & ne, int Propagate, int Error);
-  void sendEndRunInfo(const  EndRunInfo & ne, int Propagate, int Error);
-
-  virtual DPS::Producer::RunEvInfo *  getRunEvInfo(int p,int e) throw (CORBA::SystemException); 
+class AMSServerI : public AMSNode{
+ public:
+ virtual void StartClients()=0;
+ virtual void CheckClients()=0; 
+ virtual void KillClients()=0;
+ virtual AMSServerI * getServer()=0;
+  AMSServerI * next(){return dynamic_cast<AMSServerI*>(AMSNode::next());}
+  AMSServerI * prev(){return dynamic_cast<AMSServerI*>(AMSNode::prev());}
+  AMSServerI * up(){return   dynamic_cast<AMSServerI*>(AMSNode::up());}
+  AMSServerI * down(){return dynamic_cast<AMSServerI*>(AMSNode::down());}
+  AMSServerI(AMSID id):AMSNode(id){};
 };
-class Server_impl : public virtual POA_DPS::Server{
+
+class Producer_impl : public virtual POA_DPS::Producer, public AMSServerI{
+protected:
+class Prio{
+ public:
+ bool operator()(const DPS::Producer::RunEvInfo_var &a,const DPS::Producer::RunEvInfo_var &b){
+ return a->Priority<b->Priority;}
+};
+typedef  priority_queue<DPS::Producer::RunEvInfo_var,vector<DPS::Producer::RunEvInfo_var>,Prio> RunQueue;
+//typedef list<DPS::Producer::RunEvInfo> RunQueue;
+DPS::Producer_mgr _ref;
+RunQueue _rq;
+typedef list<DPS::Client::ActiveClient_var> ACL;
+ACL  _acl;
+DPS::Client::NominalClient_var  _ncl;
 public:
+ AMSServerI * getServer(){return up();}
+ virtual void StartClients();
+ virtual void CheckClients(); 
+ virtual void KillClients();
+   void sendId(const DPS::Client::CID& cid, int p, int e) throw (CORBA::SystemException);
+  void _init();
+  virtual void getACL(ACS_out acl)throw (CORBA::SystemException);
+  virtual void getNCL(NCS_out ncl)throw (CORBA::SystemException);
+  virtual TDV* getTDV(const char * name, DPS::time i, DPS::time b, DPS::time e)throw (CORBA::SystemException);
+  void sendTDV( const TDV & tdv, const char * name, DPS::time Insert, DPS::time Begin, DPS::time End, int Propagate, int  Error )throw (CORBA::SystemException);
+  void sendCurrentInfo(const  CurrentInfo &ci, int  Propagate, int Error)throw (CORBA::SystemException);
+  TDVTable * getTDVTable(const char * Name, unsigned int rv,int  Propagate, int Error)throw (CORBA::SystemException);
+  void sendNtupleEnd(const  NtupleEnd & ne, int Propagate, int Error)throw (CORBA::SystemException);
+  void sendEventStatusEnd(const  EventStatusEnd & ne, int Propagate, int Error)throw (CORBA::SystemException);
+  void sendEndRunInfo(const  EndRunInfo & ne, int Propagate, int Error)throw (CORBA::SystemException);
+
+  virtual DPS::Producer::RunEvInfo *  getRunEvInfo(int p,int e)throw (CORBA::SystemException);
+  Producer_impl(PortableServer::POA_ptr poa, char * NC=0, char* RF=0, char* NS=0, char * TS=0);
+};
+class Server_impl : public virtual POA_DPS::Server, public AMSServerI{
+protected:
+typedef list<DPS::Client::ActiveClient_var> ACL;
+ACL  _acl;
+DPS::Client::NominalClient_var _ncl;
+typedef list<DPS::Server::ActiveHost_var> AHL;
+AHL  _ahl;
+typedef list<DPS::Server::NominalHost_var> NHL;
+NHL  _nhl;
+DPS::Server_mgr _ref;
+public:
+ AMSServerI * getServer(){return this;}
+ virtual void StartClients();
+ virtual void CheckClients(); 
+ virtual void KillClients();
+  void _init(){};
   virtual void sendId(const DPS::Client::CID& cid, int p, int e) throw (CORBA::SystemException);
-  virtual void getACL(ACL_out acl);
-  virtual void getAHL(AHL_out ahl);
-  virtual void getNCL(NCL_out ncl);
-  void sendCriticalOps(const CriticalOps &, int, int);
-  void sendACL(const ACL & acl, int Propagate,int Error);
-  void sendAHL(const AHL & ahl, int Propagate,int Error);
-  void sendNCL(const NCL & ncl, int Propagate,int Error);
-  void ping();
+   void getACL(ACS_out acl)throw (CORBA::SystemException);
+   void getAHL(AHS_out ahl)throw (CORBA::SystemException);
+   void getNCL(NCS_out ncl)throw (CORBA::SystemException);
+   void sendCriticalOps(const CriticalOps &, int, int)throw (CORBA::SystemException);
+  void sendACL(const ACS & acl, int Propagate,int Error)throw (CORBA::SystemException);
+  void sendAHL(const AHS & ahl, int Propagate,int Error)throw (CORBA::SystemException);
+  void sendNCL(const NCS & ncl, int Propagate,int Error)throw (CORBA::SystemException);
+  void ping()throw (CORBA::SystemException);
+  Server_impl(PortableServer::POA_ptr poa, char * NS=0, char* NH=0);
+  ~Server_impl(){if(_down)_down->remove();}
 };
 
 
@@ -48,26 +97,15 @@ static AMSServer * _Head;
 CORBA::ORB_var  _orb; 
 PortableServer::POA_var _poa;
 PortableServer::POAManager_var _mgr;
-Producer_impl* _ppro;
-Server_impl*   _pser;
-class Prio{
- public:
- bool operator()(const DPS::Producer::RunEv &a,const DPS::Producer::RunEv &b){
- return a.Priority<b.Priority;}
-};
-typedef  priority_queue<DPS::Producer::RunEv,vector<DPS::Producer::RunEv>,Prio> RunQueue;
-RunQueue _rq;
-typedef list<DPS::Producer::RunEvInfo_var> RunEvInfo;
-RunEvInfo _rei;
-typedef list<DPS::Client::ActiveClient_var> ACL;
-ACL  _acl;
-ACL  _asl;
+AMSServerI *   _pser;
 public:
 AMSServer(int argc, char *argv[]);
 static AMSServer* & Singleton(){return _Head;};
 const CORBA::ORB_var & getOrb()const {return _orb;}
-void init();
-void run();
+void UpdateDB();
+void SystemCheck();
+~AMSServer(){if(_pser)delete _pser;}
+
 };
 
 
--- ut/CC/server.C
+++ ut/CC/server.C
@@ -1,19 +1,36 @@
+#include <stdlib.h>
 #include <server.h>
+#include <fstream.h>
 AMSServer* AMSServer::_Head=0;
 
 int main(int argc, char * argv[]){
 
  try{
     AMSServer::Singleton()=new AMSServer(argc,argv);
-    for(;;){
-     sleep(1);
-     AMSServer::Singleton()->getOrb()->perform_work();
-          
-    }
  }
- catch (CORBA::SystemException &a){
+  catch (CORBA::SystemException &a){
+   cerr <<"CorbaSystemExceptionDuringInitialization "<<endl;
    return 1;
  }
+Back:
+    for(;;){
+     try{
+      AMSServer::Singleton()->UpdateDB();
+      AMSServer::Singleton()->SystemCheck();     
+     }
+     catch(CORBA::SystemException &a){
+      cerr <<"CorbaSystemExceptionDuringUpdateDB/SystemCheck "<<endl;
+       goto Back;
+     }
+     try{
+      sleep(1);
+      AMSServer::Singleton()->getOrb()->perform_work();
+     }
+     catch(CORBA::SystemException &a){
+      cerr <<"CorbaSystemExceptionDuringPerform_Work "<<endl;
+       goto Back;
+     }
+    }
  return 0;
 }
 
@@ -23,6 +40,11 @@ AMSServer::AMSServer(int argc, char* argv[]){
  char * ior=0;
  uinteger uid=0;
  char *rfile=0;
+ char *nserver=0;
+ char *nhost=0;
+ char *nproducer=0;
+ char *ntuplestring=0;
+ char *eventtagstring=0;
  for (char *pchar=0; argc>1 &&(pchar=argv[1],*pchar=='-'); (argv++,argc--)){
     pchar++;
     switch (*pchar){
@@ -31,6 +53,21 @@ AMSServer::AMSServer(int argc, char* argv[]){
      break;
     case 'R':   //RunFileName
      rfile=++pchar;
+     break;
+    case 'H':   //NominalHostFile
+     nhost=++pchar;
+     break;
+    case 'S':   //NominalServer
+     nserver=++pchar;
+     break;
+    case 'P':   //NominalProducer
+     nproducer=++pchar;
+     break;
+    case 'N':   //NtupleOutputDir
+     ntuplestring=++pchar;
+     break;
+    case 'T':   //EventTagOutputDir
+     eventtagstring=++pchar;
      break;
     case 'U':   //uid
      uid=atoi(++pchar);
@@ -42,10 +79,10 @@ AMSServer::AMSServer(int argc, char* argv[]){
   _poa=PortableServer::POA::_narrow(obj);
   _mgr=_poa->the_POAManager();
   _mgr->activate();
-  Server_impl* _simp= new Server_impl();
+  _pser= new Server_impl(_poa,nserver,nhost);
  if(ior==0){      //  Primary
   if(rfile){  
-   Producer_impl * _pimp=new Producer_impl();
+   _pser->add(new Producer_impl(_poa,nproducer,rfile,ntuplestring,eventtagstring));
   }  
   
  }
@@ -54,3 +91,222 @@ AMSServer::AMSServer(int argc, char* argv[]){
 
 
 }
+
+Producer_impl::Producer_impl(PortableServer::POA_ptr poa, char * NC, char *RF, char *NS, char *TS): POA_DPS::Producer(),AMSServerI(AMSID("Producer",0)){
+
+ PortableServer::ObjectId_var oid=poa->activate_object(this);
+ _ref = reinterpret_cast<DPS::Producer_ptr>(poa->id_to_reference(oid));
+
+// Here read nominalclients
+   
+if(NC){
+ ifstream fbin;
+ fbin.open(NC);
+ if(fbin){
+   _ncl= new DPS::Client::NominalClient();
+   fbin>>_ncl->MaxClients>>_ncl->CPUNeeded>>_ncl->MemoryNeeded;
+   char tmpbuf[1024];
+   fbin>>tmpbuf;
+   _ncl->WholeScriptPath=(const char*)tmpbuf;
+   fbin>>tmpbuf;
+   _ncl->LogPath= (const char*)tmpbuf;
+   fbin>>tmpbuf;
+    _ncl->SubmitCommand=(const char*)tmpbuf;
+ }
+ else{
+  cerr<<"Producer_impl::Producer_impl-F-UnableToOpenNCFile "<<NC<<endl;
+  abort();
+}
+}
+//Here read runfiletable
+if(RF){
+ ifstream fbin;
+ fbin.open(RF);
+ if(fbin){
+   int cur=0;
+  while(!fbin.eof() && fbin.good()){ 
+   DPS::Producer::RunEvInfo_var  re = new DPS::Producer::RunEvInfo();
+    char tmpbuf[1024];
+   fbin>>re->Run>>re->FirstEvent>>re->LastEvent>>re->Priority>>tmpbuf;
+   re->FilePath=(const char*)tmpbuf;
+   if(re->LastEvent<re->FirstEvent)re->LastEvent=2000000000;
+   re->Status=DPS::Producer::ToBeRerun;
+   re->UpdateFreq=100;
+   re->DieHard=0;
+   re->FilePath=(const char*)NS;
+   _rq.push(re); 
+//    cout <<++cur<<" "<<re->Run<<endl;
+  }
+ }
+else{
+  cerr<<"Producer_impl::Producer_impl-F-UnableToOpenRunFile "<<RF<<endl;
+  abort();
+}
+ cout <<"RQsize "<<_rq.size()<<endl;
+}
+}
+
+Server_impl::Server_impl(PortableServer::POA_ptr poa, char* NS, char * NH): POA_DPS::Server(),AMSServerI(AMSID("Server",0)){
+//Here init asl
+
+ PortableServer::ObjectId_var oid=poa->activate_object(this);
+ _ref = reinterpret_cast<DPS::Server_ptr>(poa->id_to_reference(oid));
+if(NS){
+ ifstream fbin;
+ fbin.open(NS);
+ if(fbin){
+   DPS::Client::NominalClient_var _ncl= new DPS::Client::NominalClient();
+   fbin>>_ncl->MaxClients>>_ncl->CPUNeeded>>_ncl->MemoryNeeded;
+   char tmpbuf[1024];
+   fbin>>tmpbuf;
+   _ncl->WholeScriptPath=(const char*)tmpbuf;
+   fbin>>tmpbuf;
+   _ncl->LogPath= (const char*)tmpbuf;
+   fbin>>tmpbuf;
+    _ncl->SubmitCommand=(const char*)tmpbuf;
+ }
+ else{
+  cerr<<"Server_impl::Server_impl-F-UnableToOpenNSFile "<<NS<<endl;
+  abort();
+}
+}
+if(NH){
+ ifstream fbin;
+ fbin.open(NH);
+ if(fbin){
+  while(!fbin.eof() && fbin.good()){ 
+   DPS::Server::NominalHost_var nh= new DPS::Server::NominalHost();
+   char tmpbuf[1024];
+   fbin>>tmpbuf;
+   nh->HostName=(const char*)tmpbuf;
+   fbin>>tmpbuf;
+   nh->OS= (const char*)tmpbuf;
+   fbin>>nh->CPUNumber>>nh->Memory>>nh->Clock;
+   _nhl.push_back(nh);  
+ }
+ }
+ else{
+  cerr<<"Server_impl::Server_impl-F-UnableToOpenNHFile "<<NH<<endl;
+  abort();
+}
+}
+
+}
+
+void Producer_impl::_init(){
+// here connect to servers
+}
+
+
+void Producer_impl::sendId(const DPS::Client::CID& cid, int p, int e) throw (CORBA::SystemException){
+}
+
+
+void Producer_impl::getACL(ACS_out acl) throw (CORBA::SystemException){
+}
+
+void Producer_impl::getNCL(NCS_out ncl)throw (CORBA::SystemException){
+}
+
+DPS::Producer::TDV* Producer_impl::getTDV(const char * name, DPS::time i, DPS::time b, DPS::time e)throw (CORBA::SystemException){
+}
+
+void Producer_impl::sendTDV( const TDV & tdv, const char * name, DPS::time Insert, DPS::time Begin, DPS::time End, int Propagate, int  Error )throw (CORBA::SystemException){
+}
+
+void Producer_impl::sendCurrentInfo(const  CurrentInfo &ci, int  Propagate, int Error)throw (CORBA::SystemException){
+}
+
+  
+DPS::Producer::TDVTable * Producer_impl::getTDVTable(const char * Name, unsigned int Run ,int  Propagate, int Error)throw (CORBA::SystemException){
+}
+
+
+  void Producer_impl::sendNtupleEnd(const  NtupleEnd & ne, int Propagate, int Error)throw (CORBA::SystemException){
+}
+
+
+void Producer_impl::sendEventStatusEnd(const  EventStatusEnd & ne, int Propagate, int Error)throw (CORBA::SystemException){
+}
+
+
+void Producer_impl::sendEndRunInfo(const  EndRunInfo & ne, int Propagate, int Error) throw (CORBA::SystemException){
+}
+
+
+
+DPS::Producer::RunEvInfo *  Producer_impl::getRunEvInfo(int p,int e) throw (CORBA::SystemException){
+
+}
+
+
+
+  void Server_impl::sendId(const DPS::Client::CID& cid, int p, int e) throw (CORBA::SystemException){
+}
+
+   void Server_impl::getACL(ACS_out acl)throw (CORBA::SystemException){
+}
+
+   void Server_impl::getAHL(AHS_out ahl)throw (CORBA::SystemException){
+}
+
+   void Server_impl::getNCL(NCS_out ncl)throw (CORBA::SystemException){
+}
+
+   void Server_impl::sendCriticalOps(const CriticalOps &, int, int)throw (CORBA::SystemException){
+}
+
+  void Server_impl::sendACL(const ACS & acl, int Propagate,int Error)throw (CORBA::SystemException){
+}
+
+  void Server_impl::sendAHL(const AHS & ahl, int Propagate,int Error)throw (CORBA::SystemException){
+}
+
+  void Server_impl::sendNCL(const NCS & ncl, int Propagate,int Error)throw (CORBA::SystemException){
+}
+
+  void Server_impl::ping()throw (CORBA::SystemException){
+}
+
+
+void AMSServer::SystemCheck(){
+// Here run Start,Stop,Kill,Check Clients
+
+for(AMSServerI * pcur=_pser; pcur; pcur->down()?pcur->down():pcur->next()){
+ pcur->StartClients();
+ pcur->CheckClients();
+ pcur->KillClients();
+}
+     
+
+}
+
+void AMSServer::UpdateDB(){
+// Her don't know yet
+}
+
+
+void Producer_impl::StartClients(){
+
+
+}
+
+void Producer_impl::KillClients(){
+}
+
+void Producer_impl::CheckClients(){
+}
+
+void Server_impl::StartClients(){
+}
+
+void Server_impl::KillClients(){
+}
+
+void Server_impl::CheckClients(){
+}
+
+
+
+
+
