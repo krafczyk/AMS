6c3d592280576316b024e6cec666fefcb18f1d51
kscholbe <kscholbe>
2001-02-28 21:31:13 +0000

    Synch to new classes.

--- include/linkdef.h
+++ include/linkdef.h
@@ -1,30 +1,43 @@
-//  $Id: linkdef.h,v 1.5 2001/01/22 17:32:44 choutko Exp $
+//  $Id: linkdef.h,v 1.6 2001/02/28 21:31:13 kscholbe Exp $
 #ifdef __CINT__
 #pragma link off all globals;
 #pragma link off all classes;
 #pragma link off all functions;
 
 #pragma link C++ class BetaNtuple;
+#pragma link C++ class BetaNtuple02;
 #pragma link C++ class ChargeNtuple;
+#pragma link C++ class ChargeNtuple02;
 #pragma link C++ class ParticleNtuple;
+#pragma link C++ class ParticleNtuple02;
 #pragma link C++ class TOFClusterNtuple;
 #pragma link C++ class TOFMCClusterNtuple;
 #pragma link C++ class TrClusterNtuple;
 #pragma link C++ class TrMCClusterNtuple;
+#pragma link C++ class TRDMCClusterNtuple;
 #pragma link C++ class TrRecHitNtuple;
+#pragma link C++ class TrRecHitNtuple02;
 #pragma link C++ class TrTrackNtuple;
+#pragma link C++ class TrTrackNtuple02;
 #pragma link C++ class MCEventGNtuple;
+#pragma link C++ class MCEventGNtuple02;
 #pragma link C++ class CTCClusterNtuple;
 #pragma link C++ class CTCMCClusterNtuple;
 #pragma link C++ class AntiClusterNtuple;
 #pragma link C++ class ANTIMCClusterNtuple;
 #pragma link C++ class LVL3Ntuple;
 #pragma link C++ class LVL1Ntuple;
+#pragma link C++ class LVL1Ntuple02;
 #pragma link C++ class CTCHitNtuple;
 #pragma link C++ class TrRawClusterNtuple;
 #pragma link C++ class AntiRawClusterNtuple;
 #pragma link C++ class TOFRawClusterNtuple;
+#pragma link C++ class RICMCNtuple;
+#pragma link C++ class RICEventNtuple;
+#pragma link C++ class RICRing;
 #pragma link C++ class EcalClusterNtuple;
 #pragma link C++ class EcalHitNtuple;
 #pragma link C++ class EventNtuple;
+#pragma link C++ class EventNtuple02;
+
 #endif
