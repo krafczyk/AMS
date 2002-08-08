31ae21b1c2de16ee2fae389b174dad4633472ea9
choutko <choutko>
2002-08-08 08:00:18 +0000

    *** empty log message ***

--- CC/commons.C
+++ CC/commons.C
@@ -1,4 +1,4 @@
-//  $Id: commons.C,v 1.188 2002/07/18 13:45:46 choutko Exp $
+//  $Id: commons.C,v 1.189 2002/08/08 08:00:18 choutko Exp $
 
 
 #include <commons.h>
@@ -69,7 +69,7 @@ GCKINE_DEF GCKINE;
 // AMSDATADIR_DEF AMSDATADIR
 
 char AMSCommonsI::_version[]="v4.00";
-uinteger AMSCommonsI::_build=15;
+uinteger AMSCommonsI::_build=16;
 uinteger AMSCommonsI::_os=0;
 char AMSCommonsI::_osname[255];
 AMSCommonsI::AMSCommonsI(){
