bf123e8d6ebb350998d4b9dabdd2a45d3066fce2
schol <schol>
2002-08-15 12:22:52 +0000

    Switch to using new Root Streamers.

--- include/linkdef.h
+++ include/linkdef.h
@@ -1,42 +1,42 @@
-//  $Id: linkdef.h,v 1.17 2002/06/03 14:53:43 alexei Exp $
+//  $Id: linkdef.h,v 1.18 2002/08/15 12:22:52 schol Exp $
 #ifdef __CINT__
 #pragma link off all globals;
 #pragma link off all classes;
 #pragma link off all functions;
 
-#pragma link C++ class EventRoot02;
-#pragma link C++ class AMSEventHeaderRoot;
+#pragma link C++ class EventRoot02+;
+#pragma link C++ class AMSEventHeaderRoot+;
 
-#pragma link C++ class BetaRoot02;
-#pragma link C++ class ChargeRoot02;
-#pragma link C++ class ParticleRoot02;
-#pragma link C++ class TOFClusterRoot;
-#pragma link C++ class EcalShowerRoot;
-#pragma link C++ class EcalClusterRoot;
-#pragma link C++ class Ecal2DClusterRoot;
-#pragma link C++ class EcalHitRoot;
-#pragma link C++ class TOFMCClusterRoot;
-#pragma link C++ class TrClusterRoot;
-#pragma link C++ class TrMCClusterRoot;
-#pragma link C++ class TRDMCClusterRoot;
-#pragma link C++ class TRDRawHitRoot;
-#pragma link C++ class TRDClusterRoot;
-#pragma link C++ class TRDSegmentRoot;
-#pragma link C++ class TRDTrackRoot;
-#pragma link C++ class TrRecHitRoot02;
-#pragma link C++ class TrTrackRoot02;
-#pragma link C++ class MCTrackRoot;
-#pragma link C++ class MCEventGRoot02;
-#pragma link C++ class AntiClusterRoot;
-#pragma link C++ class ANTIMCClusterRoot;
-#pragma link C++ class LVL3Root02;
-#pragma link C++ class LVL1Root02;
-#pragma link C++ class TrRawClusterRoot;
-#pragma link C++ class AntiRawClusterRoot;
-#pragma link C++ class TOFRawClusterRoot;
-#pragma link C++ class RICMCRoot;
-#pragma link C++ class RICEventRoot;
-#pragma link C++ class RICRingRoot;
+#pragma link C++ class BetaRoot02+;
+#pragma link C++ class ChargeRoot02+;
+#pragma link C++ class ParticleRoot02+;
+#pragma link C++ class TOFClusterRoot+;
+#pragma link C++ class EcalShowerRoot+;
+#pragma link C++ class EcalClusterRoot+;
+#pragma link C++ class Ecal2DClusterRoot+;
+#pragma link C++ class EcalHitRoot+;
+#pragma link C++ class TOFMCClusterRoot+;
+#pragma link C++ class TrClusterRoot+;
+#pragma link C++ class TrMCClusterRoot+;
+#pragma link C++ class TRDMCClusterRoot+;
+#pragma link C++ class TRDRawHitRoot+;
+#pragma link C++ class TRDClusterRoot+;
+#pragma link C++ class TRDSegmentRoot+;
+#pragma link C++ class TRDTrackRoot+;
+#pragma link C++ class TrRecHitRoot02+;
+#pragma link C++ class TrTrackRoot02+;
+#pragma link C++ class MCTrackRoot+;
+#pragma link C++ class MCEventGRoot02+;
+#pragma link C++ class AntiClusterRoot+;
+#pragma link C++ class ANTIMCClusterRoot+;
+#pragma link C++ class LVL3Root02+;
+#pragma link C++ class LVL1Root02+;
+#pragma link C++ class TrRawClusterRoot+;
+#pragma link C++ class AntiRawClusterRoot+;
+#pragma link C++ class TOFRawClusterRoot+;
+#pragma link C++ class RICMCRoot+;
+#pragma link C++ class RICEventRoot+;
+#pragma link C++ class RICRingRoot+;
 #endif
 
 
