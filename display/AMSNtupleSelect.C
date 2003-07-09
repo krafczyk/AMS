8811740361e8b47d75fd798a5ee933835113c6ee
choutko <choutko>
2003-07-09 15:04:28 +0000

    *** empty log message ***

---
+++ display/AMSNtupleSelect.C
@@ -0,0 +1,26 @@
+#include "AMSNtupleHelper.h"
+extern void* gAMSUserFunction;
+extern AMSNtupleHelper* AMSNtupleHelper::fgHelper;
+class AMSNtupleSelect: public AMSNtupleHelper{
+public:
+AMSNtupleSelect(){cout <<"hhhhhhh"<<endl;};
+bool IsGolden(AMSEventR *ev){
+ if(ev && ev->nParticle()>0 && ev->nAntiCluster()>0)return true;
+  else return false;
+}
+};
+
+class AMSNtupleSelectI{
+public:
+AMSNtupleSelectI(){
+ void ** cp=(void**)0x812e44c;
+  AMSNtupleHelper::fgHelper=new AMSNtupleSelect(); 
+  cout <<(void*)&AMSNtupleHelper::fgHelper<<" "<<gAMSUserFunction<<endl; 
+}
+};
+AMSNtupleSelectI fgSelect;
+bool AMSNtupleHelper::IsGolden(AMSEventR *o){
+return true;
+}
+
+
