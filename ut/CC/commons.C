cbfb339d5279e1fe377c12c7a0de40b812b072b5
choutko <choutko>
2003-07-21 11:11:03 +0000

    *** empty log message ***

--- ut/CC/commons.C
+++ ut/CC/commons.C
@@ -1,4 +1,4 @@
-//  $Id: commons.C,v 1.241 2003/07/16 15:51:38 choutko Exp $
+//  $Id: commons.C,v 1.242 2003/07/21 11:11:03 choutko Exp $
 
 
 
@@ -72,7 +72,7 @@ GCKINE_DEF GCKINE;
 // AMSDATADIR_DEF AMSDATADIR
 
 char AMSCommonsI::_version[]="v4.00";
-uinteger AMSCommonsI::_build=88;
+uinteger AMSCommonsI::_build=89;
 uinteger AMSCommonsI::_os=0;
 char AMSCommonsI::_osname[255];
 AMSCommonsI::AMSCommonsI(){
