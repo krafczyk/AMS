fadace0ed244700e5f9591bafc7c95c67f403308
alcaraz <alcaraz>
2003-12-09 15:15:23 +0000

    *** empty log message ***

--- CC/vtx.C
+++ CC/vtx.C
@@ -77,7 +77,6 @@ integer AMSVtx::set_all(){
      // Most of the information
      number u[3];
      for (int i=0; i<_Ntracks; i++) { 
-       _Ptrack[i]->setstatus(AMSDBc::TOFFORGAMMA);
        _Ptrack[i]->setstatus(AMSDBc::USED);
 	 number rig =  _Ptrack[i]->getpirid();
 	 number en =  fabs(rig);
