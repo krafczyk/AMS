90f8346348e22be4c8413836417f7498916f6b01
choutko <choutko>
2003-12-17 16:05:36 +0000

    restore

--- examples/plot.root.C
+++ examples/plot.root.C
@@ -1,10 +1,9 @@
-/*! \example plot.root.C
- *  This is an example of how see ams variables interactively in root session
- * 
- *  (to run: root;  .x plot.root.C)   
- *  
- *  for complete listing of var functions see root_methods.h
- */
+/// \example plot.root.C
+/// This is an example of how see ams variables interactively in root session
+/// (to run: root;  .x plot.root.C)   
+/// 
+/// for complete listing of var functions see /offline/vdev/include/root_methods.h
+
 {
 gROOT->Reset();
 
@@ -15,6 +14,8 @@ gSystem->Load("/offline/vdev/lib/linux/icc/ntuple.so");
 //  gSystem->Load("/offline/vdev/lib/osf1/ntuple.so");
 //
 TChain chain("AMSRoot");
-chain.Add("/f2users/choutko/g3v1g3.root");
+chain.Add("g3v1g3.root");
 chain.Draw("ev.trtrack_rigidity(0)", "ev.trtrack_rigidity(0)>0");
 }
+
+
