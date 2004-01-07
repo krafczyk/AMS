10bc893fe58b9527a36e52471d0df03c920d2d03
choutko <choutko>
2004-01-07 17:26:29 +0000

    put correct header on run.stlv.C

--- examples/run.stlv.C
+++ examples/run.stlv.C
@@ -1,7 +1,7 @@
-/*! \example run.stlv.C
- *  This is an example of how to load stlv.C
- *  (to run: root;  .x run.stlv.C)   
- */ 
+/// \example run.stlv.C
+/// This is an example of how to load stlv.C
+///  (to run: root;  .x run.stlv.C)   
+/// 
 {
 gROOT->Reset();
 
