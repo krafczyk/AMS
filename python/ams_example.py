edb6bc28a4150a24a4dadd68aa0fccf9734368ef
alcaraz <alcaraz>
2004-02-16 18:18:27 +0000

    Small corrections to python scripts

--- python/ams_example.py
+++ python/ams_example.py
@@ -3,7 +3,7 @@
 import sys, os
 sys.path.insert(0,os.environ['AMSDir'] + '/python/linux')
 
-from AMS import *
+from AMS import TRint, TFile, TH1F, AMSChain, cvar
 
 app = TRint("My application", 0, [])
 
@@ -12,8 +12,8 @@ hfile = TFile("amstest.root","RECREATE","AMSPython test")
 hist = TH1F("hist","Z vertex position (cm)",25,-50.0,150.0)
 
 ams = AMSChain()
-ams.Add("/f2users/choutko/g3v1g3.root")
-#ams.Add("http://pcamsf0.cern.ch/f2dah1/MC/AMS02/2004A/protons/el.pl1.10200/738197524.0000001.root")
+#ams.Add("/f2users/choutko/g3v1g3.root")
+ams.Add("http://pcamsf0.cern.ch/f2dah1/MC/AMS02/2004A/protons/el.pl1.10200/738197524.0000001.root")
 ndata = ams.GetEntries()
 
 for i in range(ndata):
