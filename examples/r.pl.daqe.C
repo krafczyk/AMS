097cf01e8fec9b4731e849070203569d08e28e2f
choutko <choutko>
2009-01-05 11:39:43 +0000

    *** empty log message ***

--- examples/r.pl.daqe.C
+++ examples/r.pl.daqe.C
@@ -1,5 +1,24 @@
 {
-TProofLite *tl=new TProofLite("workers=7")        ;
+///   \example r.pl.daqe.C
+///   This is an example of how to load daqe.C (to run: root;  .x r.pl.daqe.C)
+{
+// Global ROOT Reset
+      gROOT->Reset();
+
+// Set AMSWD directory
+     gSystem->Setenv("AMSWD","/Offline/vdev");
+
+// Set include path
+      gInterpreter->AddIncludePath(gSystem->ExpandPathName("$AMSWD/include"));
+
+// Load AMS shared library
+// For Linux 2.6 load
+//      gSystem->Load("$AMSWD/lib/linux/icc2/ntuple.so");
+    gSystem->Load("$AMSWD/lib/linux/ntuple_slc4_icc.so");
+
+       //gDebug=5;
+
+TProofLite *tl=new TProofLite(" ")        ;
 TFileCollection *tf=new TFileCollection("AMSRoot");
 tf->Add("/s0fc00/Data/AMS02/2006A/data2008/cern.cosmics.sci.4p/*root");
 tf->Add("/r0fc00/Data/AMS02/2006A/data2008/cern.cosmics.sci.4p/*root");
