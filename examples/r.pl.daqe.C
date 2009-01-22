37bb04c4e1536fac8980dcbd0f85b27ee8c6c817
choutko <choutko>
2009-01-22 14:19:29 +0000

    *** empty log message ***

--- examples/r.pl.daqe.C
+++ examples/r.pl.daqe.C
@@ -22,7 +22,8 @@ TProofLite *tl=new TProofLite(" ")        ;
 TFileCollection *tf=new TFileCollection("AMSRoot");
 tf->Add("/s0fc00/Data/AMS02/2006A/data2008/cern.cosmics.sci.4p/*root");
 tf->Add("/r0fc00/Data/AMS02/2006A/data2008/cern.cosmics.sci.4p/*root");
-//tf->Add("/s0fc00/Data/AMS02/2006A/data2008/cern.cosmics.sci.4p/1207*.root");
+//  alternative way  to add files (e.g. for xrootd)
+// tf->AddFromFile("ooo");
 tl->Process(tf,"daqe.C+","oo.root");
 }
 
