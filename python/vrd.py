813c3aeaf1b52e26c2d6a426ec33da525242f723
ams <ams>
2009-02-16 17:50:07 +0000

    *** empty log message ***

--- python/vrd.py
+++ python/vrd.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python2.4
-#  $Id: vrd.py,v 1.3 2008/04/30 10:34:06 ams Exp $
+#  $Id: vrd.py,v 1.4 2009/02/16 17:50:07 ams Exp $
 
 import sys, os
 #sys.path.insert(0,os.environ['AMSDataDir'] + '/DataManagement/python/lib')
@@ -14,11 +14,15 @@ d=0
 b=0
 run2p=0
 h=0
+u=0
+#u =1 for reverse unchecked runs to tobererun & deleting ntuples
+
 for x in sys.argv:
     if x == "-h": h=1
     elif x == "-i": i=1
     elif x == "-v": v=1
     elif x == "-d": d=1
+    elif x == "-u": u=1
     elif x[0:2] == "-b":
         b=1
         if(len(x)>2):
@@ -30,6 +34,6 @@ if(d==0):
 html= RemoteClient.RemoteClient()
 html.ConnectDB(1)
 if(html.ServerConnect()):
-    html.ValidateRuns(run2p,i,v,d,h,b,1)
+    html.ValidateRuns(run2p,i,v,d,h,b,u,1)
 
 
