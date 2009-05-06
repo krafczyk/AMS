fe3609546bc6d82401fbd358e0232a4131423349
choutko <choutko>
2009-05-06 14:34:10 +0000

    add rph mag map compat (not checked)

--- F/mfield.h
+++ F/mfield.h
@@ -1,6 +1,6 @@
-*  $Id: mfield.h,v 1.12 2009/04/30 14:38:51 choutko Exp $
+*  $Id: mfield.h,v 1.13 2009/05/06 14:34:10 choutko Exp $
       integer nx,ny,nz
-      PARAMETER (nx=41,ny=41,nz=41)
+       PARAMETER (nx=41,ny=41,nz=41)
       real X(nx),y(ny),z(nz)
       real Bx(nx,ny,nz), by(nx,ny,nz),
      +          BZ(nx,ny,nz)
@@ -9,11 +9,40 @@
       real xyz(nx+ny+nz)    
       real BDx(nx,ny,nz,2), bDy(nx,ny,nz,2),
      +          BdZ(nx,ny,nz,2)
+
+
+
+      integer nrad,nphi,nzr
+      PARAMETER (nrad=8,nphi=73,nzr=23)
+      real rad(nrad),phi(nphi),zr(nzr)
+      real Bxr(nrad,nphi,nzr), byr(nrad,nphi,nzr),
+     +          BZr(nrad,nphi,nzr)
+      real Bxrc(nrad,nphi,nzr), byrc(nrad,nphi,nzr),
+     +          BZrc(nrad,nphi,nzr)
+      real xyzr(nrad+nphi+nzr)    
+      real BDxr(nrad,nphi,nzr,2), bDyr(nrad,nphi,nzr,2),
+     +          BdZr(nrad,nphi,nzr,2)
+
+
+
       integer na(3),mfile(40)
       integer isec(2),imin(2),ihour(2),iday(2),imon(2),iyear(2)
 
       common /tkfield/mfile,iniok,isec,imin,ihour,iday,imon,iyear,
-     +                na,x,y,z,bx,by,bz,xyz,bdx,bdy,bdz,bxc,byc,bzc
+     +             na,x,y,z,bx,by,bz,xyz,bdx,bdy,bdz,bxc,byc,bzc
+      equivalence (x(1),rad(1))
+      equivalence (x(9),phi(1))
+      equivalence (z(1),zr(1))
+      equivalence (xyz(1),xyzr(1))
+      equivalence (bx(1,1,1),bxr(1,1,1))
+      equivalence (by(1,1,1),byr(1,1,1))
+      equivalence (bz(1,1,1),bzr(1,1,1))
+      equivalence (bxc(1,1,1),bxrc(1,1,1))
+      equivalence (byc(1,1,1),byrc(1,1,1))
+      equivalence (bzc(1,1,1),bzrc(1,1,1))
+      equivalence (bdx(1,1,1,1),bdxr(1,1,1,1))
+      equivalence (bdy(1,1,1,1),bdyr(1,1,1,1))
+      equivalence (bdz(1,1,1,1),bdzr(1,1,1,1))
 *
 *       iniok=0  initialization
 *       iniok=-1 permanent magnet initialization
