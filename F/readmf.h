066a37cf87ed47b1f7b792f460ce9af489766225
choutko <choutko>
2009-06-02 17:32:53 +0000

    *** empty log message ***

--- F/readmf.h
+++ F/readmf.h
@@ -1,8 +1,8 @@
-*  $Id: readmf.h,v 1.1 2009/06/02 14:04:50 choutko Exp $
+*  $Id: readmf.h,v 1.2 2009/06/02 17:32:55 choutko Exp $
       integer nrad,nphi,nzr
       PARAMETER (nrad=15,nphi=72,nzr=38)
       real rad(nrad),phi(nphi),zr(nzr)
       real Br(nrad,nphi,nzr), bphi(nrad,nphi,nzr),
      +          BZ(nrad,nphi,nzr),brcoo(3,nrad,nphi,nzr),
      +          bphicoo(3,nrad,nphi,nzr),bzcoo(3,nrad,nphi,nzr)
-      common /my$/rad,phi,zr,br,bphi,bz,brcoo,bphicoo,bzcoo
+      common /my_common/rad,phi,zr,br,bphi,bz,brcoo,bphicoo,bzcoo
