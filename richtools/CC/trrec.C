2e220cea7c3134e03e2bc3cbdd76ef698aa4f2a6
mdelgado <mdelgado>
2010-05-31 10:17:06 +0000

    Bugfix

--- richtools/CC/trrec.C
+++ richtools/CC/trrec.C
@@ -9,9 +9,8 @@ TrTrack::TrTrack(TrTrackR *tr){
 
 void TrTrack::interpolate(AMSPoint pnt,AMSDir dir,AMSPoint &point,
 			  double &theta,double &phi,double &length){
-  theta=_d.gettheta();
-  phi=_d.getphi();
-  point[0]=_r[0]+tan(theta)*cos(phi)*(pnt[2]-_r[2]);
-  point[1]=_r[1]+tan(theta)*sin(phi)*(pnt[2]-_r[2]);
-  point[2]=pnt[2];
+  // Find the impact point with the plane given by pnt and dir
+
+  double t=(pnt-_r).prod(dir)/_d.prod(dir);
+  point=_r+_d*t;
 }
