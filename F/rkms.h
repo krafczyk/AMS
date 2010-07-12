89a24fc1ecefc0f4f5b9181e1a5ddee1b3e2e02c
choutko <choutko>
2010-07-12 14:12:17 +0000

    *** empty log message ***

--- F/rkms.h
+++ F/rkms.h
@@ -1,32 +1,20 @@
-*  $Id: rkms.h,v 1.9 2010/03/24 10:19:03 choutko Exp $
-      integer npma
-      parameter(NPma=9) 
-      integer npoc
-      double precision xc,yc,zc,wxy,Dhi2
-      common/rkms_1/xc(NPma),yc(NPma),zc(NPma),wxy(NPma,NPma),Dhi2,npoc
-      common/rkms_init/trkz0
+*============================================================= rkms.h
+*===  AMS02 2010 (Permanent magnet & Tracker with 9 Si-planes)
+*=============================================================
+      integer   NPma ! maximum number of planes
+      parameter(NPma=9)
+*     ------------------------------------------- /rkms_1/
+      integer npoc  !  number of planes with hits
+      double precision xc,yc,zc,wx,wy,Dhi2
+      common/rkms_1/xc(NPma),yc(NPma),zc(NPma)
+     +     ,wx(NPma,NPma),wy(NPma,NPma),Dhi2,npoc
+*     ------------------------------------------- /rkst/
       double precision rkstep
-      common /rkms_2/xt,yt,wt
-      double precision xt(NPma),yt(NPma),wt(3,npma)
       common/rkst/     rkstep
-!$OMP threadprivate(/rkms_1/,/rkms_2/,/rkst/)
-      real SiThick,zz0
-      data SiThick/0.0300/    ! plane thicknes in cm. (300mkm Si)
-      data     zz0/0.0032054/ ! plane thicknes in R.L.(300mkm Si)
-
+!$OMP threadprivate(/rkms_1/,/rkst/)
+*     --------------------------------------- position of Si-planes
+      common/rkms_init/trkZ0 ! (taken from MC call rkmsinit)
       real trkZ0(NPma)
-*=== AMS01 Nov.29,1996
-c01      data trkZ0/50.95,29.15,7.75,-7.75,-29.15,-50.95/
-*=== AMS02 (Aug.25,04)
-c      data trkZ0/54.7,29.2,25.8,1.7,-1.7,-25.8,-29.2,-54.7/
-*   taken from MC (Sep.04)
-c      data trkZ0/54.51,29.18,25.32,1.68,-2.18,-25.32,-29.18,-54.51/
-c      data trkZ0/52.985,29.185,25.215,1.685,-2.285,
-c     + -25.215,-29.185,-52.985,-122./
-
-      save SiThick,zz0
-*-------------------------------------------------------
-*   pl#   1-2   2-3   3-4   4-5   5-6   6-7  7-8
-*   dZ   21.8  24.4  15.5  24.4  21.8   -    -    AMS01
-*   dZ   25.5   3.4  24.1   3.4  24.1  3.4  25.5  AMS02
-*-------------------------------------------------------
+c      data trkZ0                                                   !#c
+c     + /166.92,54.51,29.18,25.32,1.68,-2.18,-25.32,-29.18,-135.48/ !#c
+*     ------------------------------------------------------------
