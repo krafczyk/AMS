3511ef240488cb4e25dfbd0f03319c5526b48558
mdelgado <mdelgado>
2010-08-11 16:57:42 +0000

    *** empty log message ***

--- richtools/CC/trrec.C
+++ richtools/CC/trrec.C
@@ -2,8 +2,14 @@
 
 
 TrTrack::TrTrack(TrTrackR *tr){
+#ifndef _PGTRACK_
   _r.setp(tr->P0[0],tr->P0[1],tr->P0[2]);
   _d=AMSDir(tr->Theta,tr->Phi);
+#else
+  _r=tr->GetP0();
+  _d=tr->GetDir();
+#endif
+
 }
 
 
