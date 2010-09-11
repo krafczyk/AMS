bd4b1911f9e4836aa817e65f4bf211e55e7995c9
pzuccon <pzuccon>
2010-09-11 13:33:42 +0000

    Add include for analysis

---
+++ include.orig/root_RVSP.h
@@ -0,0 +1,12 @@
+//  $Id: root_RVSP.h,v 1.1 2010/09/11 13:33:42 pzuccon Exp $
+#ifndef  __WRITEROOT__
+#define __WRITEROOT__
+#define __ROOTPOINTERS__
+#endif
+#ifndef  __ROOTSHAREDLIBRARY__
+#define __ROOTSHAREDLIBRARY__
+#endif
+#ifndef _PGTRACK_
+#define _PGTRACK_
+#endif
+#include "root.h"
