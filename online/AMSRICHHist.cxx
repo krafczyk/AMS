ce532e3263bd336f1344acf240e7e9eede27869d
mdelgado <mdelgado>
2011-03-24 15:26:41 +0000

    Simplifying definition of histograms

--- online/AMSRICHHist.cxx
+++ online/AMSRICHHist.cxx
@@ -1,4 +1,4 @@
-//  $Id: AMSRICHHist.cxx,v 1.8 2008/04/11 09:52:29 mdelgado Exp $
+//  $Id: AMSRICHHist.cxx,v 1.9 2011/03/24 15:26:41 mdelgado Exp $
 #include <iostream>
 #include "AMSDisplay.h"
 #include <TGraphErrors.h>
@@ -11,109 +11,69 @@
 #include "AMSRICHHist.h"
 
 
+int AMSRICHHist::FindName(TString name){
+  for(int i=0;i<_filled.size();i++) if(name==_filled[i]->GetName()) return i;
+  return -1;
+}
 
 
 void AMSRICHHist::Book(){ 
 
-AddSet("RICH Raw");       // About occupancy and so on
+  AddSet("RICH Raw");       // About occupancy and so on without refering to recsontrcution
 
-  _filled.push_back(new TH1F("RICHOccupancy","RICH Number of hits",16*680,0,16*680));
-  _filled[_filled.size()-1]->SetXTitle("Rich hits");
-  _filled[_filled.size()-1]->SetYTitle("Number of events");
-  _filled[_filled.size()-1]->SetFillColor(41);
+  // Macros to avoid writing too much
+#define pushH1(_name,_title,_n,_min,_max) _filled.push_back(new TH1F(#_name,#_title,_n,_min,_max))
+#define pushProf(_name,_title,_n,_min,_max) _filled.push_back(new TProfile(#_name,#_title,_n,_min,_max))
+#define _LAST _filled[_filled.size()-1]
+#define setAxisNames(_xtitle,_ytitle,_fillcolor) {_LAST->SetXTitle(#_xtitle);_LAST->SetYTitle(#_ytitle);_LAST->SetFillColor(_fillcolor);}
+#define get(_name) _filled[FindName(#_name)]
 
+  pushH1(RICHOccupancy,RICH Number of hits,16*680,0,16*680);
+  setAxisNames(Rich hits,Number of events,41);
 
-
-  _filled.push_back(new TH1F("RICHOccupancy2","RICH Occupancy per channel",16*680,0,16*680));
-  _filled[_filled.size()-1]->SetXTitle("Rich soft Channel");
-  _filled[_filled.size()-1]->SetYTitle("Number of hits");
-  _filled[_filled.size()-1]->SetFillColor(41);
-
-
+  pushH1(RICHOccupancy2,RICH Occupancy per channel,16*680,0,16*680);
+  setAxisNames(Rich Channel,Number of hits,41);
+  
   AddSet("RICH Hits");      // About hits information (signals and so on)
 
-  _filled.push_back(new TH1F("RICHSignal","RICH Charge",500,0,100));
-  _filled[_filled.size()-1]->SetXTitle("Number of photoelectrons");
-  _filled[_filled.size()-1]->SetFillColor(41);
+  pushH1(RICHSignal,RICH Hit Charge,500,0,100);
+  setAxisNames(Number of photoelectrons,,41);
+
+  pushH1(RICHADC1,RICH ADC gainx1,1024,0,4096);
+  setAxisNames(ADC_{#times 1} count,,41);
 
 
-  _filled.push_back(new TH1F("RICHADC1","RICH ADC gainx1",1024,0,4096));
-  _filled[_filled.size()-1]->SetXTitle("ADC_{#times 1} count");
-  _filled[_filled.size()-1]->SetFillColor(41);
-
-  _filled.push_back(new TH1F("RICHADC5","RICH ADC gainx5",1024,0,4096));
-  _filled[_filled.size()-1]->SetXTitle("ADC_{#times 5} count");
-  _filled[_filled.size()-1]->SetFillColor(41);
-
+  pushH1(RICHADC5,RICH ADC gainx5,1024,0,4096);
+  setAxisNames(ADC_{#times 5} count,,41); 
 
   AddSet("RICH Rings");     // About reconstructed rings
-  
-  _filled.push_back(new TH1F("RICHRings","RICH Nb. of Rings",30,0,30));
-  _filled[_filled.size()-1]->SetXTitle("RICH rings");
-  _filled[_filled.size()-1]->SetFillColor(41);
 
-  _filled.push_back(new TH1F("RICHRingProb","RICH Ring probability",50,0.,1.));
-  _filled[_filled.size()-1]->SetXTitle("Ring probability");
-  _filled[_filled.size()-1]->SetFillColor(41);
+  pushH1(RICHRings,RICH Nb. of Rings,30,0,30);
+  setAxisNames(RICH rings,,41);
 
-  _filled.push_back(new TH1F("RICHBeta","RICH Beta spectrum",50,0.97,1.01));
-  _filled[_filled.size()-1]->SetXTitle("#beta_{RICH}(prob>0.001)");
-  _filled[_filled.size()-1]->SetFillColor(41);
+  pushH1(RICHRingProb,RICH Ring probability,50,0.,1.);
+  setAxisNames(Ring probability,,41);
 
-  _filled.push_back(new TH1F("RICHCharge","RICH Charge spectrum",100,0.,10.));
-  _filled[_filled.size()-1]->SetXTitle("#Z_{RICH}(prob>0.001)");
-  _filled[_filled.size()-1]->SetFillColor(41);
+  pushH1(RICHBeta,RICH Beta spectrum,50,0.97,1.01);
+  setAxisNames(#beta_{RICH}(prob>0.001),,41);
+
+  pushH1(RICHCharge,RICH Charge spectrum,100,0.,10.);
+  setAxisNames(#Z_{RICH}(prob>0.001),,41);
 
   AddSet("RICH alignment");     // About RICH/Tracker alignment
   
-  /*
-  _filled.push_back(new TProfile("TrXvsRichX","Tracker X vs Rich X",70,-70,70));
-  _filled[_filled.size()-1]->SetXTitle("Tracker X vs Rich X");
-  _filled[_filled.size()-1]->SetFillColor(41);
 
-  _filled.push_back(new TProfile("TrYvsRichY","Tracker Y vs Rich Y",70,-70,70));
-  _filled[_filled.size()-1]->SetXTitle("Tracker Y vs Rich Y");
-  _filled[_filled.size()-1]->SetFillColor(41);
+  pushProf(TrXvsRichX,Tracker X vs Rich X,70,-70,70);
+  setAxisNames(Tracker X (cm),RICH X-Tracker X (cm),41);
 
+  pushProf(TrYvsRichY,Tracker Y vs Rich Y,70,-70,70);
+  setAxisNames(Tracker Y (cm),RICH Y-Tracker Y (cm),41);
 
-  _filled.push_back(new TProfile("TrX-RichX","Tracker X-Rich X vs cosine track",100,0,1));
-  _filled[_filled.size()-1]->SetXTitle("cos #theta for track");
-  _filled[_filled.size()-1]->SetYTitle("Tracker X-Rich X");
-  _filled[_filled.size()-1]->SetFillColor(41);
+  pushH1(TrX-RichX,Tracker X-Rich X,200,-12,12);
+  setAxisNames(Tracker X-Rich X (cm),,41);
 
-  _filled.push_back(new TProfile("TrY-RichY","Tracker Y-Rich Y vs cosine track",100,0,1));
-  _filled[_filled.size()-1]->SetXTitle("cos #theta for track");
-  _filled[_filled.size()-1]->SetYTitle("Tracker Y-Rich Y");
-  _filled[_filled.size()-1]->SetFillColor(41);
-  */
-
-  _filled.push_back(new TProfile("TrXvsRichX","Tracker X vs Rich X",70,-70,70));
-  _filled[_filled.size()-1]->SetXTitle("Tracker X");
-  _filled[_filled.size()-1]->SetYTitle("RICH X-Tracker X");
-  _filled[_filled.size()-1]->SetFillColor(41);
-
-  _filled.push_back(new TProfile("TrYvsRichY","Tracker Y vs Rich Y",70,-70,70));
-  _filled[_filled.size()-1]->SetXTitle("Tracker Y");
-  _filled[_filled.size()-1]->SetYTitle("RICH Y-Tracker Y");
-  _filled[_filled.size()-1]->SetFillColor(41);
-
-
-  _filled.push_back(new TH1F("TrX-RichX","Tracker X-Rich X",200,-12,12));
-  _filled[_filled.size()-1]->SetXTitle("Tracker X-Rich X");
-  _filled[_filled.size()-1]->SetFillColor(41);
-
-  _filled.push_back(new TH1F("TrY-RichY","Tracker Y-Rich",200,-12,12));
-  _filled[_filled.size()-1]->SetXTitle("Tracker Y-Rich Y");
-  _filled[_filled.size()-1]->SetFillColor(41);
-
-
-
-  //  AddSet("RICH/TRACKER"); // Mass reconstruction
-
-  //  _filled.push_back(new TH1F("RICHMass","RICH Mass ",100,0.,10.));
-  //  _filled[_filled.size()-1]->SetXTitle("Mass (prob>0.01)");
-  //  _filled[_filled.size()-1]->SetFillColor(41);
-
+  pushH1(TrY-RichY,Tracker Y-Rich,200,-12,12);
+  setAxisNames(Tracker Y-Rich Y (cm),,41);
 }
 
 
@@ -188,72 +148,71 @@ void AMSRICHHist::ShowSet(Int_t Set){
 
 void AMSRICHHist::Fill(AMSNtupleR *ntuple){ 
   if(ntuple->nLevel1()){
-    _filled[0]->Fill(ntuple->nRichHit(),1.);
+    get(RICHOccupancy)->Fill(ntuple->nRichHit());
     for(int i=0;i<ntuple->nRichHit();i++){
       RichHitR current=ntuple->RichHit(i);
-      _filled[1]->Fill(current.Channel,1.);
-
-      _filled[2]->Fill(current.Npe,1.);
+      get(RICHOccupancy2)->Fill(current.Channel);
+      
+      get(RICHSignal)->Fill(current.Npe);
       int gain_mask=1<<29;
       if(! (current.Status&gain_mask))
-	_filled[3]->Fill(current.Counts,1.);
+	get(RICHADC1)->Fill(current.Counts);
       else
-	_filled[4]->Fill(current.Counts,1.);
+	get(RICHADC5)->Fill(current.Counts);
     }
-    _filled[5]->Fill(ntuple->nRichRing(),1.);
+    get(RICHRings)->Fill(ntuple->nRichRing(),1.);
+
     if(ntuple->nParticle()==1){
       ParticleR current=ntuple->Particle(0);
       if(current.pRichRing())
 	{
-	  _filled[6]->Fill(current.pRichRing()->Prob,1.);
-	  if(current.pRichRing()->Prob>1e-3){
-	    _filled[7]->Fill(current.pRichRing()->BetaRefit,1.);
-	    if(current.pRichRing()->NpExp>0)
-	      _filled[8]->Fill(sqrt((current.pRichRing()->NpCol)/
-				    (current.pRichRing()->NpExp)),1.);
+	  get(RICHRingProb)->Fill(current.pRichRing()->Prob);
 
+	  if(current.pRichRing()->Prob>1e-3){
+	    get(RICHBeta)->Fill(current.pRichRing()->getBeta());
+	    if(current.pRichRing()->NpExp>0)
+	      get(RICHCharge)->Fill(sqrt((current.pRichRing()->getPhotoElectrons())/
+					 (current.pRichRing()->NpExp)));
+	    
 	    // Compute preliminary alignment histograms: search a crossed PMT
 	    int ring_number=current.iRichRing();
 	    float *crossingpoint=current.pRichRing()->TrPMTPos;
-
+	    
 	    
 	    static float pmtx[680],pmty[680],entries[680];
 	    memset(pmtx,0,sizeof(pmtx));
 	    memset(pmty,0,sizeof(pmty));
 	    memset(entries,0,sizeof(entries));
-
+	    
 	    int mask=1<<30;                               // Mask of crossed channels
-
+	    
 	    // Search of crossed pmts
 	    for(int i=0;i<ntuple->nRichHit();i++){
 	      RichHitR &hit=ntuple->RichHit(i);
 	      if(!(hit.Status&mask)) continue;
-
+	      
 	      int pmt=hit.Channel/16;
 	      entries[pmt]+=hit.Npe;
 	      pmtx[pmt]+=hit.Coo[0]*hit.Npe; 
 	      pmty[pmt]+=hit.Coo[1]*hit.Npe; 
-
 	    }
 	    
 	    int winner=-1;
 	    float winnerdist=1e6;
-
+	    
 	    for(int i=0;i<680;i++){
 	      if(entries[i]==0) continue;
 	      pmtx[i]/=entries[i];
 	      pmty[i]/=entries[i];
-
-
 	      
 	      float dist=(pmtx[i]-crossingpoint[0])*(pmtx[i]-crossingpoint[0])+(pmty[i]-crossingpoint[1])*(pmty[i]-crossingpoint[1]);
-
+	      
 	      if(dist<winnerdist)
 		{
 		  winnerdist=dist;
 		  winner=i;
 		}
-
+	      
 	    }
 	     
 	    if(winner!=-1){
@@ -262,16 +221,12 @@ void AMSRICHHist::Fill(AMSNtupleR *ntuple){
 		(current.pRichRing()->TrPMTPos[1]-current.pRichRing()->TrRadPos[1])*(current.pRichRing()->TrPMTPos[1]-current.pRichRing()->TrRadPos[1])+
 		(current.pRichRing()->TrPMTPos[2]-current.pRichRing()->TrRadPos[2])*(current.pRichRing()->TrPMTPos[2]-current.pRichRing()->TrRadPos[2]);
 	      cosine=sqrt((current.pRichRing()->TrPMTPos[2]-current.pRichRing()->TrRadPos[2])*(current.pRichRing()->TrPMTPos[2]-current.pRichRing()->TrRadPos[2])/cosine);
-		
-
-	      //	      ((TProfile*)_filled[9])->Fill(pmtx[winner],crossingpoint[0]);
-	      //	      ((TProfile*)_filled[10])->Fill(pmty[winner],crossingpoint[1]);
-	      ((TProfile*)_filled[9])->Fill(crossingpoint[0],pmtx[winner]-crossingpoint[0]);
-	      ((TProfile*)_filled[10])->Fill(crossingpoint[1],pmty[winner]-crossingpoint[1]);
-	      //	      ((TProfile*)_filled[11])->Fill(crossingpoint[0]-pmtx[winner],cosine);
-	      //	      ((TProfile*)_filled[12])->Fill(crossingpoint[1]-pmty[winner],cosine);
-	      ((TH1F*)_filled[11])->Fill(crossingpoint[0]-pmtx[winner]);
-	      ((TH1F*)_filled[12])->Fill(crossingpoint[1]-pmty[winner]);
+	      
+	      
+	      get(TrXvsRichX)->Fill(crossingpoint[0],pmtx[winner]-crossingpoint[0]);
+	      get(TrYvsRichY)->Fill(crossingpoint[1],pmty[winner]-crossingpoint[1]);
+	      get(TrX-RichX)->Fill(crossingpoint[0]-pmtx[winner]);
+	      get(TrY-RichY)->Fill(crossingpoint[1]-pmty[winner]);
 	    }
 	    
 
--- online/AMSRICHHist.h
+++ online/AMSRICHHist.h
@@ -1,9 +1,12 @@
 #include "AMSHist.h"
 
 class AMSRICHHist : public AMSHist{
+private:
+int FindName(TString name);
 public:
 AMSRICHHist(Text_t * name, Text_t * title, Int_t active=1):AMSHist(name,title,active){};
 void Book();
 void ShowSet(int );
 void Fill(AMSNtupleR *ntuple);
+ 
 };
