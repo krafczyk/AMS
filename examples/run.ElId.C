e1f5dd43cab1f556cb1bf2cf461fb11e77130d22
mmilling <mmilling>
2011-03-25 14:19:43 +0000

    *** empty log message ***

--- examples/run.ElId.C
+++ examples/run.ElId.C
@@ -3,11 +3,11 @@
 {
       gROOT->Reset();
 
-      gInterpreter->AddIncludePath(gSystem->ExpandPathName("$AMSDir/include"));
-      gSystem->Load("$AMSDir/lib/linuxx8664gcc/ntuple_slc4_PG.so");
+      gInterpreter->AddIncludePath(gSystem->ExpandPathName("${AMSDir}/include"));
+      gSystem->Load(gSystem->ExpandPathName("${AMSDir}/lib/linuxx8664icc/ntuple_slc4_PG.so"));
 
       TChain chain("AMSRoot");
-      chain.Add("/fcdat1/Data/AMS02/2009B/bt.aug2010.v5.p2/el100/1282150717.00000002.root");
+      chain.Add("/fcdat1/Data/AMS02/2009B/bt.aug2010.v5.p3/el100/1282150717.00000001.root");
       
       // Either define the output ROOT file ("select.root") here:
       chain.Process("ElectronIdentification.C+","select.root");
