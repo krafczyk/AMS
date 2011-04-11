a8a8832d951cbe5d2a8b63ea9774b5fcfec396c5
dmitrif <dmitrif>
2011-04-11 10:27:21 +0000

    *** empty log message ***

--- perl/lib/RemoteClient.pm
+++ perl/lib/RemoteClient.pm
@@ -1,4 +1,4 @@
-# $Id: RemoteClient.pm,v 1.648 2011/04/11 10:09:58 choutko Exp $
+# $Id: RemoteClient.pm,v 1.649 2011/04/11 10:27:21 dmitrif Exp $
 #
 # Apr , 2003 . ak. Default DST file transfer is set to 'NO' for all modes
 #
@@ -113,7 +113,6 @@ use POSIX  qw(strtod);
 use File::Find;
 use Benchmark;
 use Class::Struct;
-use List::Util qw[min max];
 
 @RemoteClient::EXPORT= qw(new  datasetlink Connect  Warning ConnectDB ConnectOnlyDB  checkDB listAll listMCStatus listMin listShort queryDB04 DownloadSA castorPath  checkJobsTimeout deleteTimeOutJobs deleteDST  getEventsLeft getHostsList getHostsMips getOutputPath getProductionPeriods getRunInfo updateHostInfo parseJournalFiles prepareCastorCopyScript resetFilesProcessingFlag ValidateRuns updateAllRunCatalog printMC02GammaTest readDataSets set_root_env updateCopyStatus updateHostsMips checkTiming list_24h_html test00 RemoveFromDisks  UploadToDisks CheckCRC MoveBetweenDisks UploadToCastor GroupRuns TestPerl );
 
@@ -721,7 +720,9 @@ jpid=productionset.did and realtriggers>0 and cid=".$self->{CCID}." and producti
             $max_jobs = 0;
         }
         if($completed_jobs > 0){
-            $max_jobs=max($max_jobs,$capacity_jobs-$total_jobs+$completed_jobs);
+	    if($max_jobs < $capacity_jobs-$total_jobs+$completed_jobs){
+		$max_jobs = $capacity_jobs-$total_jobs+$completed_jobs;
+            }
         }
           }
         }
