58e7e8b6b66930cd2c84fadf7a44e055b633802b
jizhang <jizhang>
2011-05-26 19:41:05 +0000

    *** empty log message ***

--- DTMonitor/DTMonitor.py
+++ DTMonitor/DTMonitor.py
@@ -6,19 +6,21 @@ from array import array
 import os
 import time
 #options
-folder = "/s0dat0/FRAMES/RS422" #default folder
+folder = "/fc02dat0/FRAMES/SCIBPB/RT" #default folder
+folder2 = "/fc02dat0/FRAMES/SCI/RT" #default folder2
 
-c1 = TCanvas( 'c1', 'File Monitor', 200, 10, 1000, 900 )
+c1 = TCanvas( 'c1', 'Frame Monitor', 200, 10, 1000, 900 )
 c1.SetLogy(1)
 
 #saves[ 'c1' ] = c1
 
 def DrawFilesPlot(timewin):
-	global folder
+	global folder,folder2
 	c1.Clear()
-# pick a folder you have ...
+
 	filesR_size,filesR_cdate = [],[]
 	filesH_size,filesH_cdate = [],[]
+
 	for (path, dirs, files) in os.walk(folder):
 	    for file in files:
 	        filename = os.path.join(path, file)
@@ -28,13 +30,19 @@ def DrawFilesPlot(timewin):
 			if cdate > timewin - 60*60:
 				filesR_size.append(fsize)
 				filesR_cdate.append(cdate)
-			if cdate > timewin - 60*60*24*7:
-				filesH_size.append(fsize)
-				filesH_cdate.append(cdate)
 	fileRlist=zip(filesR_cdate,filesR_size)
 	fileRlist.sort()
-	fileHlist=zip(filesH_cdate,filesH_size)
-	fileHlist.sort()
+        for (path, dirs, files) in os.walk(folder2):
+            for file in files:
+                filename = os.path.join(path, file)
+                fsize=os.path.getsize(filename)
+                cdate=os.path.getctime(filename)
+                if not filename.startswith("lastfile"):
+                        if cdate > timewin - 60*60:
+                                filesH_size.append(fsize)
+                                filesH_cdate.append(cdate)
+        fileHlist=zip(filesH_cdate,filesH_size)
+        fileHlist.sort()
 
 	#Draw plot
 #	c1 = TCanvas( 'c1', 'Sizes of Frame Files', 200, 10, 1000, 900 )
@@ -44,7 +52,7 @@ def DrawFilesPlot(timewin):
 #	pad1 = TPad( 'pad1', 'Frame Sizes (1 hour)', 0.02, 0.02, 0.48, 0.83, 33 )
 #s	pad2 = TPad( 'pad2', 'Frame Sizes (1 hour)', 0.52, 0.02, 0.98, 0.83, 33 )
 
-#draw 1 hour plot
+#draw 1 hour plot /SCIBPB/RT
 	c1.cd(1)
 	gPad.SetGrid()
 	gPad.SetLogy(1)
@@ -75,7 +83,7 @@ def DrawFilesPlot(timewin):
 	gr2.SetLineWidth( 4 )
 	gr2.SetMarkerColor( 4 )
 	gr2.SetMarkerStyle( 21 )
-	gr2.SetTitle( '1-Hour Plot' )
+	gr2.SetTitle( '1-Hour Plot (SCIBPB/RT)' )
 	gr2.GetXaxis().SetTitle( 'Date' )
 	gStyle.SetTimeOffset(0); 
 	gr2.GetXaxis().SetTimeDisplay(1)
@@ -90,7 +98,7 @@ def DrawFilesPlot(timewin):
 	gPad.Draw()	
 	gPad.Modified();
 
-#draw 48 hour plot
+#draw 1 hour plot /SCI/RT
 	c1.cd(2)
 	gPad.SetGrid()
 	gPad.SetLogy(1)
@@ -110,8 +118,8 @@ def DrawFilesPlot(timewin):
 		pt2.SetFillColor(0) 
 		pt2.SetTextSize(0.04) 
 		pt2.SetTextAlign(12)
-		pt2.AddText("No incoming frame in the past week")
-	x1.append(timewin-3600*24*7-3600)
+		pt2.AddText("No incoming frame in the past hour")
+	x1.append(timewin-7200)
 	y1.append(0)
 	x1.append(timewin-3600)
 	y1.append(0)
@@ -121,7 +129,7 @@ def DrawFilesPlot(timewin):
 	gr1.SetLineWidth( 4 )
 	gr1.SetMarkerColor( 4 )
 	gr1.SetMarkerStyle( 21 )
-	gr1.SetTitle( '1-Week Plot' )
+	gr1.SetTitle( '1-hour Plot (SCI/RT) ' )
 	gr1.GetXaxis().SetTitle( 'Date' )
 	gr1.GetXaxis().SetTimeDisplay(1)
 	#gr.GetXaxis().SetNdivisions(-60)
@@ -129,7 +137,7 @@ def DrawFilesPlot(timewin):
 	gr1.GetYaxis().SetTitle( 'Size [(KB]' )
 	gr1.GetXaxis().SetLabelSize(.03)
 
-	gr1.SetMarkerSize(.6)
+	#gr1.SetMarkerSize(.6)
 
 	gr1.Draw('AP')
   	if nofileH== True:
