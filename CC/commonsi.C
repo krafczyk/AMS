eb8e463ef19800462fc778c4b3edff6a50c1cf1f
choutko <choutko>
2011-06-06 15:09:16 +0000

    b518 tag

--- CC/commonsi.C
+++ CC/commonsi.C
@@ -1,4 +1,4 @@
-//  $Id: commonsi.C,v 1.157.2.5 2011/06/03 19:01:43 choutko Exp $
+//  $Id: commonsi.C,v 1.157.2.6 2011/06/06 15:09:16 choutko Exp $
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/time.h>
@@ -28,7 +28,7 @@ char AMSCommonsI::_version[]="v4.00";
 #else
 char AMSCommonsI::_version[]="v5.00";
 #endif
-uinteger AMSCommonsI::_build=517;
+uinteger AMSCommonsI::_build=518;
 uinteger AMSCommonsI::_buildtime=0;
 float AMSCommonsI::_mips=1000;
 uinteger AMSCommonsI::_os=0;
