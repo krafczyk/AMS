6030702c9148a4622fe8c60ccb9bfef953480e6f
pzuccon <pzuccon>
2011-08-26 09:33:10 +0000

    correction to documentation

--- include/root.h
+++ include/root.h
@@ -1,4 +1,5 @@
-//  $Id: root.h,v 1.384 2011/08/25 14:23:46 pzuccon Exp $
+//  $Id: root.h,v 1.385 2011/08/26 09:33:10 pzuccon Exp $
+
 //
 //  NB 
 //  Only stl vectors ,scalars and fixed size arrays 
@@ -2431,13 +2432,13 @@ public:
    return type;
   }
 #ifdef _PGTRACK_
-  /// Returns the extrapolation on the TrackerLayer J-scheme (1-9)
+  /// Returns the extrapolation on the TrackerLayer OLD scheme (1-9)
   float GetTrCoo(int lay,int coo) const {
     return GetTrCooJ(TkDBc::Head->GetJFromLayer(lay),coo);
   }
-  /// Returns the extrapolation on the TrackerLayer OLD scheme (1-9)
-  float GetTrCooJ(int lay,int coo) const {
-    return TrCoo[lay-1][coo];
+  /// Returns the extrapolation on the TrackerLayer J-scheme (1-9)
+  float GetTrCooJ(int jlay,int coo) const {
+    return TrCoo[jlay-1][coo];
   }
 #endif
   /// \param number index in container
