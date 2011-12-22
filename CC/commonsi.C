e3967042e955498aade62bda8e650505c5a18139
choutko <choutko>
2011-12-22 09:37:35 +0000

    increase buld number after ecal bug fixing

--- CC/commonsi.C
+++ CC/commonsi.C
@@ -1,4 +1,4 @@
-//  $Id: commonsi.C,v 1.177.2.1 2011/12/07 18:07:01 choutko Exp $
+//  $Id: commonsi.C,v 1.177.2.2 2011/12/22 09:37:35 choutko Exp $
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/time.h>
@@ -26,7 +26,7 @@ char AMSCommonsI::_version[]="v4.00";
 #else
 char AMSCommonsI::_version[]="v5.00";
 #endif
-uinteger AMSCommonsI::_build=551;
+uinteger AMSCommonsI::_build=552;
 uinteger AMSCommonsI::_buildtime=0;
 float AMSCommonsI::_mips=1000;
 uinteger AMSCommonsI::_os=0;
