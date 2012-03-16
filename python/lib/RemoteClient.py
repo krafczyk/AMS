b2ba96b1e3cd7e4ad820b9bdd5f5f66ea7e429c9
choutko <choutko>
2012-03-16 09:25:42 +0000

    *** empty log message ***

--- python/lib/RemoteClient.py
+++ python/lib/RemoteClient.py
@@ -1781,6 +1781,8 @@ class RemoteClient:
         output='/castor/cern.ch/ams'
         buf2=input.split(buf[1])
         output=output+buf2[1]
+        cmd="nsrm "+output
+        cmdstatus=os.system(cmd)
         cmd="/afs/cern.ch/ams/local/bin/timeout --signal 9 1200 /afs/cern.ch/exp/ams/Offline/root/Linux/527.icc64/bin/xrdcp "+input+" root://castorpublic.cern.ch//"+output
         cmdstatus=os.system(cmd)
         if(cmdstatus):
