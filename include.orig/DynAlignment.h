2fd330993adfd66d78d362ce1a21f931d0a348e7
choutko <choutko>
2012-05-08 13:57:13 +0000

    fix for the mt

--- include.orig/DynAlignment.h
+++ include.orig/DynAlignment.h
@@ -302,8 +302,10 @@ class DynAlManager:public TObject{
 					       float &rotZ,float &rotY,float &rotX,float &dx,float &dy,float &dz);
 
 
-#pragma omp threadprivate(dynAlFitContainers,currentRun,skipRun,tdvBuffer,tdvdb,begin,insert,end)  
-
+#ifdef __ROOTSHAREDLIBREY__
+#pragma omp threadprivate(dynAlFitContainers)
+#pragma omp threadprivate (currentRun,skipRun,tdvBuffer,tdvdb,begin,insert,end)  
+#endif
   ClassDef(DynAlManager,4);
 };
 
