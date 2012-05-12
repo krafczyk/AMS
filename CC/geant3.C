7a7bb8d6c07514436126a1c1f1836a388db4823b
barao <barao>
2012-05-12 07:14:32 +0000

    copy of arrays on different threads ensured through pragma on geant3.C: badpix_ams,badpix_lip,badpmt_lip

--- CC/geant3.C
+++ CC/geant3.C
@@ -1,4 +1,4 @@
-//  $Id: geant3.C,v 1.152.2.1 2012/05/05 10:07:51 choutko Exp $
+//  $Id: geant3.C,v 1.152.2.2 2012/05/12 07:14:32 barao Exp $
 
 #include "typedefs.h"
 #include "cern.h"
@@ -960,7 +960,7 @@ nchunk=omp_get_num_procs()*10;
 for(int ik=0;ik<maxt;ik++)ia[ik*16]=0; 
 //cout <<"  new chunk "<<nchunk<<endl;
 
-#pragma omp parallel  default(none),shared(cpulimit,std::cout,std::cerr,amsffkey_,selectffkey_,gcflag_,run,event,tt,oldtime,count,nchunk,ia,Waiting), private(pdaq), copyin(LIPC2F) 
+#pragma omp parallel  default(none),shared(cpulimit,std::cout,std::cerr,amsffkey_,selectffkey_,gcflag_,run,event,tt,oldtime,count,nchunk,ia,Waiting), private(pdaq), copyin(LIPC2F,badpix_ams,badpix_lip,badpmt_lip) 
 {
 
    AMSEvent::ResetThreadWait(1);
