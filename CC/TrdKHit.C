059fec6651851cd22b898be7bf3176b47e014a9d
zweng <zweng>
2012-06-19 08:08:47 +0000

    TrdKLikelihood Methods, version 1.0

---
+++ CC/TrdKHit.C
@@ -0,0 +1,251 @@
+#include "TrdKHit.h"
+
+ClassImp(TrdKHit)
+
+
+float TrdKHit::Tube_radius=0.303;
+
+TrdKHit::TrdKHit(){}
+TrdKHit::~TrdKHit(){}
+
+TrdKHit::TrdKHit(TrdRawHitR *_trdhit, float zshit){
+
+    TRDHit_Layer=_trdhit->Layer;
+    TrdHCalibR::gethead()->GetTubeIdFromLLT(_trdhit->Layer,_trdhit->Ladder,_trdhit->Tube,tubeid);
+    mf=TrdHCalibR::gethead()->GetManifold(tubeid/128);
+    if(TRDHit_Layer<=3 || TRDHit_Layer>=16)TRDHit_Direction=1;
+    else TRDHit_Direction=0;
+    IsCalibrated=0;
+
+    TRDHit_Amp=_trdhit->Amp;
+    TRDHitRZD rzd(*_trdhit);
+    TRDHit_z=rzd.z;
+    TRDRawHit_Z=rzd.z+zshit;
+    TRDRawHit_R=rzd.r;
+    if(TRDHit_Direction==0){
+        TRDHit_x=rzd.r;
+        TRDHit_y=0;
+        TRDTube_Center = AMSPoint(TRDHit_x,0,TRDHit_z);
+        TRDTube_Dir = AMSPoint(0,1,0);
+    }else{
+        TRDHit_y=rzd.r;
+        TRDHit_x=0;
+        TRDTube_Center = AMSPoint(0,TRDHit_y,TRDHit_z);
+        TRDTube_Dir = AMSPoint(1,0,0);
+    }
+
+    TRDTube_Center_Raw=TRDTube_Center;
+    TRDTube_Dir_Raw=TRDTube_Dir;
+
+    IsAligned=0;
+
+
+    _rawhit=_trdhit;
+
+}
+
+void TrdKHit::DoGainCalibration(float corr){
+    TRDHit_GainCorrection=corr;
+    TRDHit_Amp*=corr;
+    IsCalibrated=1;
+}
+
+void TrdKHit::DoAlignment(TRDAlignmentPar *par){
+
+    AMSPoint T(par->dX,par->dY,par->dZ);
+
+    AMSRotMat R;
+    R.SetRotAngles(-1*par->alpha,-1*par->beta,-1*par->gamma);
+
+    //    cout<<"Rotation: "<<R<<endl;
+    AMSPoint Center(par->RotationCenter_X,par->RotationCenter_Y,par->RotationCenter_Z);
+
+    //    AMSPoint temp_TRDHit(TRDHit_x,TRDHit_y,TRDHit_z);
+    //    AMSPoint TRDHit_Local=temp_TRDHit-Center;
+    //    AMSPoint TRDHit_Aligned=R*TRDHit_Local+T+Center;
+    //    TRDHit_x=TRDHit_Aligned.x();
+    //    TRDHit_y=TRDHit_Aligned.y();
+    //    TRDHit_z=TRDHit_Aligned.z();
+
+
+    AMSPoint TRDTube_Local=TRDTube_Center-Center;
+    TRDTube_Center=R*TRDTube_Local+T+Center;
+    TRDTube_Dir=R*TRDTube_Dir;
+
+
+    Aligned_RotationMatrix=R;
+    Aligned_Translation=T;
+    Aligned_Center = Center;
+
+
+    IsAligned=1;
+}
+
+
+
+void TrdKHit::PredictCrossing(AMSPoint *P0, AMSDir *u, int tracktype){
+
+    //=========
+    //
+    // http://softsurfer.com/Archive/algorithm_0106/algorithm_0106.htm
+    //
+    //
+    //==============
+
+    AMSPoint w0=*P0-TRDTube_Center;
+
+    float a=1;
+    float b=(*u).prod(TRDTube_Dir);
+    float c=1;
+    float d=(*u).prod(w0);
+    float e=TRDTube_Dir.prod(w0);
+
+    float denorm=a*c-b*b;
+    if(denorm==0)cout<<"ERROR: Track Tube in Parrarel orientation"<<endl;
+    float sc=(b*e-c*d)/denorm;
+    float tc=(a*e-b*d)/denorm;
+
+    AMSPoint hit=TRDTube_Center+TRDTube_Dir*tc;
+    //    cout<<"Tube: Position:"<<TRDTube_Center<<", Dir: "<<TRDTube_Dir<<endl;
+    //    cout<<"Track: Position:"<<(*P0)<<", Dir: "<<(*u)<<endl;
+    //    cout<<"Hit: "<<hit<<endl;
+
+    if(tracktype==0){
+        TRDHit_x=hit.x();
+        TRDHit_y=hit.y();
+        TRDHit_z=hit.z();
+    }else if(tracktype==1){
+        TRDHit_TRDTrack_x=hit.x();
+        TRDHit_TRDTrack_y=hit.y();
+        TRDHit_TRDTrack_z=hit.z();
+    }else{
+        cout<<"~~~WARNING~~~TrdKHit from TrdKCluster, warn track type when getting Hit position~~~"<<endl;
+    }
+
+    return;
+
+}
+
+double TrdKHit::Tube_Track_Distance_3D(AMSPoint *C2, AMSDir *D2){
+    AMSPoint diff=(*C2)-TRDTube_Center;
+    AMSPoint cross=(*D2).crossp(TRDTube_Dir);
+    return ((diff.prod(cross)))/cross.norm();
+}
+
+double TrdKHit::Tube_Track_Distance_2D(AMSPoint *C2, AMSDir *D2){
+
+    if(TRDHit_Direction==0){
+        double kx=D2->x()/D2->z();
+        double x=C2->x()+kx*(TRDRawHit_Z-C2->z());
+        double l=TMath::Abs(x-TRDRawHit_R);
+        return l*sqrt(1/(1+kx*kx));
+    }else if(TRDHit_Direction==1){
+        double ky=D2->y()/D2->z();
+        double y=C2->y()+ky*(TRDRawHit_Z-C2->z());
+        double l=TMath::Abs(y-TRDRawHit_R);
+        return l*sqrt(1/(1+ky*ky));
+    }
+
+    return 999;
+}
+
+double TrdKHit::Tube_Track_3DLength_New(double d, AMSDir *Dir){
+    double ll=Tube_radius*Tube_radius-d*d;
+    if(ll<=0)return 0;
+    double l=sqrt(ll);
+    double kx=Dir->x()/Dir->z();
+    double ky=Dir->y()/Dir->z();
+    if(TRDHit_Direction==0)    return 2*sqrt(l*l*(1+ky*ky/(1+kx*kx)));
+    else return 2*sqrt(l*l*(1+kx*kx/(1+ky*ky)));
+}
+
+double TrdKHit::Tube_Track_3DLength_New(AMSPoint *X0, AMSDir *Dir){
+    double d=Tube_Track_Distance_2D(X0,Dir);
+    return Tube_Track_3DLength_New(d,Dir);
+}
+
+double TrdKHit::Tube_Track_3DLength(AMSPoint *X0, AMSDir *Dir){
+
+    float tubelength=190;  //======Relistic tube lenght dimention needed====
+
+    AMSRotMat RCor=Aligned_RotationMatrix;
+    RCor.Invert();
+
+    AMSPoint X0prime=RCor*(*X0-Aligned_Translation-Aligned_Center)+Aligned_Center-TRDTube_Center_Raw;
+    AMSDir DirPRIME=RCor*(*Dir);
+
+    double x0,y0,dx,dy;
+    if(TRDHit_Direction==1){
+        x0=X0prime.y();
+        y0=X0prime.z();
+        dx=DirPRIME.y();
+        dy=DirPRIME.z();
+    }else if(TRDHit_Direction==0){
+        x0=X0prime.x();
+        y0=X0prime.z();
+        dx=DirPRIME.x();
+        dy=DirPRIME.z();
+    }
+
+    long double t1,t2;
+    long double A=pow(dx,2)+pow(dy,2);
+    long double B=2*(y0*dy + x0*dx);
+    long double C = pow(x0,2) + pow(y0,2) - pow(Tube_radius,2);
+    long double D = pow(B,2) - 4*A*C;
+
+
+    //    cout<<"A,B,C,D: "<<A<<", "<<B<<", "<<C<<", "<<D<<endl;
+
+    if(D<=0)return 0;
+    t1=(-1*B+sqrt(D))/(2*A);
+    t2=(-1*B-sqrt(D))/(2*A);
+    //    cout<<"t1,t2: "<<t1<<", "<<t2<<endl;
+    AMSPoint P1=X0prime+DirPRIME*t1;
+    AMSPoint P2=X0prime+DirPRIME*t2;
+
+    //    cout<<"P1: "<<P1<<"  P2: "<<P2<<endl;
+
+
+    if(TRDHit_Direction==1){
+        if(abs(P1.x())>tubelength && abs(P2.x())>tubelength){
+            return 0;
+        }
+        if(abs(P1.x())>tubelength){
+            if(P1.x()>0){
+                float deltax=abs(P1.x()-tubelength);
+                P1=P1-DirPRIME*(deltax/DirPRIME.x());
+            }
+        }
+        if(abs(P2.x())>tubelength){
+            if(P2.x()>0){
+                float deltax=abs(P2.x()-tubelength);
+                P2=P2-DirPRIME*(deltax/DirPRIME.x());
+            }
+        }
+    }
+
+    if(TRDHit_Direction==0){
+        if(abs(P1.y())>tubelength && abs(P2.y())>tubelength){
+            return 0;
+        }
+        if(abs(P1.y())>tubelength){
+            if(P1.y()>0){
+                long double deltax=abs(P1.y()-tubelength);
+                P1=P1-DirPRIME*(deltax/DirPRIME.y());
+            }
+        }
+        if(abs(P2.y())>tubelength){
+            if(P2.y()>0){
+                long double deltax=abs(P2.y()-tubelength);
+                P2=P2-DirPRIME*(deltax/DirPRIME.y());
+            }
+        }
+    }
+
+
+    long double distance=(P1-P2).norm();
+
+
+
+    return distance;
+}
---
+++ include/TrdKPDF.h
@@ -0,0 +1,345 @@
+//**************************************************************//
+// TRDKLikelihood methods
+//
+// For detailed information:
+// https://twiki.cern.ch/twiki/bin/view/AMS/TRDKLikelihood
+//
+//
+// 2012-06-18  v1.0,  First commit on cvs
+//
+//**************************************************************//
+
+#ifndef TrdKPDF_H
+#define TrdKPDF_H
+
+#include "TROOT.h"
+#include "Rtypes.h"
+#include "math.h"
+#include "stdio.h"
+#include "stdlib.h"
+#include "iostream"
+#include "TF1.h"
+#include "TMath.h"
+#include "TString.h"
+#include "TH1F.h"
+#include "TRandom3.h"
+using namespace std;
+
+class TrdKPDF:public TObject{
+
+public:
+
+    TrdKPDF(TString s){
+        s_particle=s;
+        if(s=="Proton"){
+            particle=1;
+            Init_Proton();
+        }else if(s=="Electron"){
+            particle=0;
+            Init_Electron();
+        }
+        else if(s=="Helium"){
+            particle=2;
+            Init_Helium();
+        }else if(s=="Nuclei"){  //Added by Wei Sun
+            particle=3;
+            Init_Nuclei();
+        }
+        else {
+            std::cout<<"Particle: "<<s<<" Not Supported "<<std::endl;
+        }
+
+
+        SetNormalization(0,0);
+
+        f_func = new TF1("func_for_random",this,0,4000,0,"TrdKPDF");
+        f_func->SetNpx(4000);
+
+
+    }
+    ~TrdKPDF(){};
+
+
+    void Init_Proton();
+    void Init_Electron();
+    void Init_Helium();
+    void Init_Nuclei();      //Added by Wei Sun
+
+
+
+    void GetPar(float Pmom, float Length, int Layer, float Press){
+        //        cout<<"KPDF, GetPar: "<<Pre_Layer<<endl;
+        Pre_Momentum=Pmom;
+        Pre_PathLength=Length;
+        Pre_Xe=Press*1000;
+        Pre_Layer=Layer;
+
+        if(particle==0)GetPar_Electron(Pmom,Length,Layer,Press);
+        else if(particle==1)GetPar_Proton(Pmom,Length,Layer,Press);
+        else if(particle==2)GetPar_Helium(Pmom,Length,Layer,Press);
+
+    }
+
+    void GetPar_Proton(Double_t Pmom, Double_t Length, Int_t Layer, Double_t Press);
+    void GetPar_Electron(Double_t Pmom, Double_t Length, Int_t Layer, Double_t Press);
+    void GetPar_Helium(Double_t Pmom, Double_t Length, Int_t Layer, Double_t Press);
+    void GetPar_Nuclei(Double_t Charge, Double_t Pmom, Double_t Length, Int_t Layer, Double_t Press);   //Added by Wei Sun
+
+
+
+    void SetPar_1TeV(){
+        if(particle==0){
+            Double_t _par[11]={0.434965, 114.903360, 80.460000, 0.500000, 0.565035, 442.466750, 413.437120, 0.207120, 600.000000, 700.000000, 0.500000};
+            memcpy(Par,_par,sizeof(Double_t)*11);
+            return;
+        }
+
+        if(particle==1){
+            Double_t _par[11]={0.700000, 114.903360, 80.460000, 0.500000, 0.300000, 400.000000, 300.000000, 0.207120, 600.000000, 700.000000, 0.500000};
+            memcpy(Par,_par,sizeof(Double_t)*11);
+            return;
+        }
+    }
+
+    void SetPar_2TeV(){
+        if(particle==0){
+            Double_t _par[11]={0.434965, 114.903360, 80.460000, 0.500000, 0.565035, 442.466750, 413.437120, 0.207120, 600.000000, 700.000000, 0.500000};
+            memcpy(Par,_par,sizeof(Double_t)*11);
+            return;
+        }
+        if(particle==1){
+            Double_t _par[11]={0.500000, 114.903360, 80.460000, 0.500000, 0.500000, 442.466750, 320.000000, 0.250000, 600.000000, 700.000000, 0.500000};
+            memcpy(Par,_par,sizeof(Double_t)*11);
+            return;
+        }
+    }
+
+
+
+    Double_t func_li(Double_t x, Double_t a, Double_t b, Double_t c);
+    Double_t func_l(Double_t x, Double_t a, Double_t b, Double_t c);
+
+    Double_t GetNormalization(double start);
+    Double_t GetLikelihood(double Amp, float Corr=1.0);
+    Double_t GetLikelihood(double Amp,float Pmom,float Length,int Layer,float Press){
+        GetPar(Pmom,Length,Layer,Press);
+        return GetLikelihood(Amp);
+    }
+
+
+
+    double operator() (double *x, double *p) {
+        return GetLikelihood(x[0]);
+    }
+
+
+    vector<float> Generate(int nevents){
+        vector<float> hits;
+        TF1 *f = new TF1("temp_f",this,0,4000,0,"TrdKPDF");
+        f->SetNpx(4000);
+        for(int i=0;i<nevents;i++){
+            hits.push_back(f->GetRandom());
+        }
+        delete f;
+        return hits;
+    }
+
+    float GenerateHit(int start=-999){
+
+        float Corr=1.0;
+
+        float Norm=1.0;
+        if(start>0)Norm=GetNormalization(start);
+        //        if(Normalization_Method==0) Norm=1.0;
+
+        double Prob = gRandom.Uniform(0,1);
+
+        Float_t Amp, Amp1, Amp2;
+        Double_t x, v;
+
+        x = 15.*Corr;
+        v = 1.-GetNormalization(x)/Norm;
+        if ( Prob < v ) return 0;
+        Amp1 = (Int_t)x;
+
+        x = 3400.*Corr;
+        v = 1.-GetNormalization(x)/Norm;
+        if ( Prob >= v ) return x;
+        Amp2 = (Int_t)x;
+
+        Amp = (Amp1+Amp2)/2;
+
+        float Amp_previous=0;
+
+        while ( fabs(Amp-Amp_previous)>0.1 ) {
+            Amp_previous=Amp;
+            x = Corr*Amp;
+            v = 1.-GetNormalization(x)/Norm;
+            if ( Prob >= v ) Amp1 = Amp;
+            else             Amp2 = Amp;
+            Amp = (Amp1+Amp2)/2;
+            x = Corr*Amp;
+        }
+
+        return x;
+    }
+
+
+
+    TH1F* DrawFunc(){
+        TString s_name=Form("h_TrdKPDF_M%.0f_P%.2f_Xe%.0f_L%i_"+s_particle,Pre_Momentum,Pre_PathLength,Pre_Xe,Pre_Layer);
+        cout<<s_name<<endl;
+
+        s_name.ReplaceAll(".","_");
+        TF1 *f = new TF1(s_name,this,-100,4000,0,"TrdKPDF");
+        f->SetNpx(4100);
+        TH1F* h=(TH1F*)f->GetHistogram();
+        h->SetTitle(s_name);
+        h->SetName(s_name);
+
+        return h;
+    }
+
+
+    void SetSeed(int seed){gRandom.SetSeed(seed);}
+
+
+
+    TRandom3 gRandom;
+
+    TF1* f_func;
+
+    TString s_particle;
+    int particle;
+
+
+    Double_t mom[50];
+    Double_t p01_m[50];
+    Double_t p02_m[50];
+    Double_t p03_m[50];
+    Double_t p04_m[50];
+    Double_t p05_m[50];
+    Double_t p06_m[50];
+    Double_t p07_m[50];
+    Double_t p08_m[50];
+    Double_t p09_m[50];
+    Double_t p10_m[50];
+    Double_t p11_m[50];
+    Double_t p12_m[50];
+    Double_t p13_m[50];
+    Double_t p14_m[50];
+    Double_t p15_m[50];
+
+    Double_t len[18];
+    Double_t p01_l[18];
+    Double_t p02_l[18];
+    Double_t p03_l[18];
+    Double_t p04_l[18];
+    Double_t p05_l[18];
+    Double_t p06_l[18];
+    Double_t p07_l[18];
+    Double_t p08_l[18];
+    Double_t p09_l[18];
+    Double_t p10_l[18];
+    Double_t p11_l[18];
+    Double_t p12_l[18];
+    Double_t p13_l[18];
+    Double_t p14_l[18];
+    Double_t p15_l[18];
+
+
+    Double_t p01_n[20];
+    Double_t p02_n[20];
+    Double_t p03_n[20];
+    Double_t p04_n[20];
+    Double_t p05_n[20];
+    Double_t p06_n[20];
+    Double_t p07_n[20];
+    Double_t p08_n[20];
+    Double_t p09_n[20];
+    Double_t p10_n[20];
+    Double_t p11_n[20];
+    Double_t p12_n[20];
+    Double_t p13_n[20];
+    Double_t p14_n[20];
+    Double_t p15_n[20];
+
+    Double_t c01_x[18];
+    Double_t c02_x[18];
+    Double_t c03_x[18];
+    Double_t c04_x[18];
+    Double_t c05_x[18];
+    Double_t c06_x[18];
+    Double_t c07_x[18];
+    Double_t c08_x[18];
+    Double_t c09_x[18];
+    Double_t c10_x[18];
+    Double_t c11_x[18];
+    Double_t c12_x[18];
+    Double_t c13_x[18];
+    Double_t c14_x[18];
+    Double_t c15_x[18];
+
+
+
+    Double_t Par[15];
+
+
+    float Pre_Momentum;
+    float Pre_PathLength;
+    float Pre_Xe;
+    int Pre_Layer;
+
+
+
+
+
+
+
+    int Normalization_Method;
+    double Normalization_Start;
+    double Norm;
+
+    void SetNormalization(int _Method, double start=-9999){
+        Normalization_Method=_Method;
+        Normalization_Start=start;
+//        Norm=GetNormalization(Normalization_Start);
+    }
+
+
+
+
+    //=========For TRD Charge Estimation==========
+
+    Double_t chrg[80];
+    Double_t p02_c[80];
+    Double_t p03_c[80];
+    Double_t p04_c[80];
+    Double_t p09_c[80];
+    Double_t p10_c[80];
+
+    Double_t GetLikelihoodQ(double Amp);
+    Double_t GetLikelihoodHA(Double_t Corr);
+    Double_t GetLikelihoodLA(Double_t Corr);
+    Double_t GetLikelihood(Double_t Amp,Double_t Charge,Double_t Corr,Double_t Pmom,Double_t Length,Int_t Layer,Double_t Press){
+        GetPar_Nuclei(Charge,Pmom,Length,Layer,Press);
+        if(Amp>3400*Corr) return GetLikelihoodHA(Corr);
+        else if(Amp<15*Corr) return GetLikelihoodLA(Corr);
+        else return GetLikelihoodQ(Amp);
+    }
+
+    Double_t GetLikelihoodDR(Double_t DAmpL,Double_t Charge,Double_t Rigidity);
+
+
+
+
+        ClassDef(TrdKPDF,1)
+
+};
+
+
+
+
+
+
+#endif // TrdKPDF_H
