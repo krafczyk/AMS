a1e15369978a6b3539c5ecbae0da7427faafe654
mduranti <mduranti>
2012-06-23 21:12:40 +0000

    minor bug fix

--- include/AMSNtupleHelper.h
+++ include/AMSNtupleHelper.h
@@ -1,7 +1,11 @@
 #ifndef __AMSNtupleHelper__
 #define __AMSNtupleHelper__
 
+#ifdef _PGTRACK_
+#include "root_RVSP.h"
+#else
 #include "root_RVS.h"
+#endif
 class AMSNtupleHelper{
 public:
  AMSNtupleHelper(){};
