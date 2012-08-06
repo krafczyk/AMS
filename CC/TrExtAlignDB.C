9ad1dae9182a3708b73dcc4f6ad78d1636fe452f
pzuccon <pzuccon>
2012-08-06 18:58:54 +0000

    temporary patch for performance on periods with no alignment yet

--- CC/TrExtAlignDB.C
+++ CC/TrExtAlignDB.C
@@ -206,7 +206,14 @@ return -5;
 
 int  TrExtAlignDB::UpdateTkDBcDyn(int run,uint time, int pln,int lad1,int lad9){
   if(DynAlManager::ignoreAlignment) return 0;
-
+// PZ -- FIXME -- TO be removed and replaced by TDV entry with years validity and zero content
+static int nprint=0;
+  if(time > 1337450000 ){
+    if(nprint++<10) printf("TrExtAlignDB::UpdateTkDBcDyn-W- Warning no dyn alignment available after 1337450000, this message will be repeted only 10 times \n");
+    SL1[6]=SL1[7]=SL1[8]=SL1[9]=SL1[10]=SL1[11]=0.;
+    SL9[6]=SL9[7]=SL9[8]=SL9[9]=SL9[10]=SL9[11]=0.;
+    return 0;
+  }
   if (!TkDBc::Head) {
     std::cerr << "TkDBc::Head is null" << std::endl;
     return -2;
@@ -402,6 +409,14 @@ int  TrExtAlignDB::UpdateTkDBc(uint time) const
     return -1;
   }
 
+// PZ -- FIXME -- TO be removed and replaced by TDV entry with years validity and zero content
+  static int nprint=0;
+  if(time > 1337450000 ){
+    if(nprint++<10) printf("TrExtAlignDB::UpdateTkDBc-W- Warning no dyn alignment available after 1337450000, this message will be repeted only 10 times \n");
+    SL1[0]=SL1[1]=SL1[2]=SL1[3]=SL1[4]=SL1[5]=0.;
+    SL9[0]=SL9[1]=SL9[2]=SL9[3]=SL9[4]=SL9[5]=0.;
+    return 0;
+  }
 
   int errlim=100;
   static int nwar = 0;
