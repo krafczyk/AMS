3a1e10d8d9f64ccf4a4a4dd216cc146f7c9eaa16
baosong <baosong>
2012-08-21 21:26:30 +0000

    added by baosong: SOC Consle program as the remote shell of rdlastraw.perl

---
+++ perl/glastraw_exp.pl
@@ -0,0 +1,87 @@
+#!/usr/bin/perl -w
+
+use Tk;
+use Tk::Font;
+use strict;
+
+my $mw = MainWindow->new;
+my  $font = $mw->Font(-family=> 'arial', -size  => 20);
+$mw->geometry("400x160");
+$mw->title("SOC Console");
+my $status;
+my $message;
+my $time;
+my $lastupdate;
+my $output;
+my $clastupdate="";
+my $updateinterval = 600000;
+my $timerefreshinterval = 60000;
+my $ssherrorcount = 0;
+my $footLabel = $mw->Label(-textvariable => \$time, -borderwidth => 2,
+           -relief => 'groove')->pack(-fill => 'x',
+                                      -side => 'bottom');
+my $messageLabel = $mw->Label(-textvariable => \$message, -font => $font, -relief => 'groove', -background => 'yellow')->pack(-fill => 'x', -side => 'top', -expand => 0);
+my $statusLabel = $mw->Label(-textvariable => \$status, -font => $font, -relief => 'groove')->pack(-fill => 'both', -expand => 1);
+
+my $defaultcolor = $messageLabel->cget( -background );
+
+refreshStatus();
+timerefresh();
+
+MainLoop;
+
+sub timerefresh{
+    my $minupdated = int((time() - $lastupdate) / 60);
+     my $ms=$minupdated<2?"":"s";
+    $time = $minupdated==0?"Last update: $clastupdate":"Last update: $clastupdate;  $minupdated minute$ms ago";
+    $mw->after($timerefreshinterval, \&timerefresh);
+}
+
+sub refreshStatus{
+ $clastupdate=localtime();
+    $time = "Last update $clastupdate" ;
+    $lastupdate = time();
+    $output = `timeout 30 ssh amslocal\@pcamsf4 "cd /Offline/vdev/perl/; ./rdlastraw.perl -ok @ARGV"`;
+    chomp $output;
+    if ($output eq "") {
+        $statusLabel->configure(-background => "red");
+        my $pingmsg = `ping -c 3 pcamsf4 | grep transmitted | awk '{print \$4}'`;
+        chomp($pingmsg);
+        if ($pingmsg eq '0') {
+            $status = 'Cannot reach pcamsf4';
+            $message = 'Call expert: 16 4733';
+        }
+        else {
+            if ($ssherrorcount > 1) {
+                $status = 'Cannot ssh pcamsf4';
+                $message = 'Call expert: 16 9642';
+            }
+            else {
+                $ssherrorcount++;
+            }
+        }
+    }
+    else {
+        $ssherrorcount = 0;
+        my @outarray = split(/,/, $output);
+        $message = $outarray[0];
+    
+        $outarray[1] =~ s/ OK//g;
+        $outarray[1] =~ s/^ //g; 
+        $outarray[1] =~ s/ $//g; 
+        if ($outarray[1] =~ m/^$/) {
+            $statusLabel->configure(-background => "green");
+            $status = 'OK';
+        }
+        else {
+            $statusLabel->configure(-background => "red");
+            chomp($outarray[1]);
+            $outarray[1] =~ s/ /\n/g;
+            $status = $outarray[1];
+        }
+    }
+
+    $mw->update;
+    $mw->after($updateinterval, \&refreshStatus);
+    $mw->after($timerefreshinterval, \&timerefresh);
+}
