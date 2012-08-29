b20f7b76a5fcae078beeeb3bad3d4182e3cfb6bc
barao <barao>
2012-08-29 18:25:39 +0000

    added getacceptance method to richutil.F package; build number increased

--- CC/commonsi.C
+++ CC/commonsi.C
@@ -1,4 +1,4 @@
-//  $Id: commonsi.C,v 1.204 2012/08/13 16:59:46 choutko Exp $
+//  $Id: commonsi.C,v 1.205 2012/08/29 18:25:39 barao Exp $
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/time.h>
@@ -26,7 +26,7 @@ char AMSCommonsI::_version[]="v4.00";
 #else
 char AMSCommonsI::_version[]="v5.00";
 #endif
-uinteger AMSCommonsI::_build=600;
+uinteger AMSCommonsI::_build=601;
 uinteger AMSCommonsI::_buildtime=0;
 float AMSCommonsI::_mips=1000;
 uinteger AMSCommonsI::_os=0;
