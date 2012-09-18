169414457d03a95919564027f888c8b7d21b67f7
choutko <choutko>
2012-09-18 08:47:02 +0000

    add build no to 590 - production version ready

--- CC/commonsi.C
+++ CC/commonsi.C
@@ -1,4 +1,4 @@
-//  $Id: commonsi.C,v 1.196.2.4 2012/06/09 21:40:50 pzuccon Exp $
+//  $Id: commonsi.C,v 1.196.2.5 2012/09/18 08:47:02 choutko Exp $
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/time.h>
@@ -26,7 +26,7 @@ char AMSCommonsI::_version[]="v4.00";
 #else
 char AMSCommonsI::_version[]="v5.00";
 #endif
-uinteger AMSCommonsI::_build=589;
+uinteger AMSCommonsI::_build=590;
 uinteger AMSCommonsI::_buildtime=0;
 float AMSCommonsI::_mips=1000;
 uinteger AMSCommonsI::_os=0;
