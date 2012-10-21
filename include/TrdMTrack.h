9f4af90a45d093c8357350e082daf173d0426c15
mheil <mheil>
2012-10-21 10:15:20 +0000

    added accessor to pathlength and residuals

--- include/TrdMTrack.h
+++ include/TrdMTrack.h
@@ -58,11 +58,13 @@ class TrdMTrack : public TObject
   
   // Hit accessor
   TrdRawHitR* pTrdMHit(int i){if(i>NHits() || i<0)return 0;return TrdMHits.at(i);}
-  
+  float HitPathlength(int i){if(i>NHits() || i<0)return 0;return phitlen.at(i);}
+  AMSPoint HitResidual(int i){if(i>NHits() || i<0){AMSPoint err(0.,0., 0.); return err;} return hitres.at(i);}
+ 
   // for processing
   float GetGainCorrection(int module, int tube=0);
   float GetXePressure();
- 
+  
   // Globlal Validity flag
   int GetValidity(){return IsValid;}
   
@@ -79,13 +81,13 @@ class TrdMTrack : public TObject
   // for processing
   void SetAlignment();
   int which_tube(int lay, float loc, int mod);
-  float GetPathLength3D(AMSPoint trdpnt, AMSDir trddir, AMSPoint tpnt, AMSDir tdir );
   void interpolate_trd_track(TrdTrackR* trdtrack, AMSPoint &pnt, AMSDir &dir, int z);
+  float GetPathLength3D(AMSPoint trdpnt, AMSDir trddir, AMSPoint tpnt, AMSDir tdir );
   float get_trd_rz(int layer, int ladder, int tube, int mod);
   double GetProProb(int xeindex, int sindex, float amp);
-  double GetProProb(float amp, float rig, float pl, float lay, float xe );
+  double GetProProb(float amp, float rig, float pl, int lay, float xe );
   double GetElProb(int xeindex, int sindex, float amp);
-  double GetElProb(float amp, float rig, float pl, float lay, float xe );
+  double GetElProb(float amp, float rig, float pl, int lay, float xe );
 
   // additional Initalisation
   void Init_Alignment();
@@ -131,6 +133,7 @@ class TrdMTrack : public TObject
   
   vector<TrdRawHitR*> TrdMHits;
   vector<float> phitlen;
+  vector<AMSPoint> hitres;
   
   ClassDef(TrdMTrack,1);
     
