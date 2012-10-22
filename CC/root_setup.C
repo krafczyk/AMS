4eb22446d7255ea4380e8a28d0ad08bfd253a498
choutko <choutko>
2012-10-22 08:03:39 +0000

    update STK path to v0.09.ECI_STK_2012

--- CC/root_setup.C
+++ CC/root_setup.C
@@ -1,4 +1,4 @@
-//  $Id: root_setup.C,v 1.99.2.2 2012/10/20 19:33:23 choutko Exp $
+//  $Id: root_setup.C,v 1.99.2.3 2012/10/22 08:03:39 choutko Exp $
 #include "root_setup.h"
 #include "root.h"
 #include <fstream>
@@ -1147,7 +1147,7 @@ else if(t2-t1>864000){
     cerr<< "AMSSetupR::LoadAMSSTK-S-EndBeginDifferenceTooBigMax864000 "<<t2-t1<<endl;
    t2=t1+864000;
 }
-const char fpatb[]="ECI_STK_";
+const char fpatb[]="v0.09.ECI_STK_";
 const char fpate[]=".csv";
 
     tm tmf;
