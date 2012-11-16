9f8a860aa79a939ba54a07da604703bab92be2bd
choutko <choutko>
2012-11-16 09:13:35 +0000

    build 622

--- CC/commonsi.C
+++ CC/commonsi.C
@@ -1,4 +1,4 @@
-//  $Id: commonsi.C,v 1.213.2.1 2012/11/13 12:59:24 choutko Exp $
+//  $Id: commonsi.C,v 1.213.2.2 2012/11/16 09:13:35 choutko Exp $
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/time.h>
@@ -26,7 +26,7 @@ char AMSCommonsI::_version[]="v4.00";
 #else
 char AMSCommonsI::_version[]="v5.00";
 #endif
-uinteger AMSCommonsI::_build=621;
+uinteger AMSCommonsI::_build=622;
 uinteger AMSCommonsI::_buildtime=0;
 float AMSCommonsI::_mips=1000;
 uinteger AMSCommonsI::_os=0;
