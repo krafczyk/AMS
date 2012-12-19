7099f9276e599feaab716ef4a043a37c61531447
choutko <choutko>
2012-12-19 15:29:48 +0000

    put vdev to build 641

--- CC/commonsi.C
+++ CC/commonsi.C
@@ -1,4 +1,4 @@
-//  $Id: commonsi.C,v 1.215 2012/12/11 17:50:42 qyan Exp $
+//  $Id: commonsi.C,v 1.216 2012/12/19 15:29:48 choutko Exp $
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/time.h>
@@ -26,7 +26,7 @@ char AMSCommonsI::_version[]="v4.00";
 #else
 char AMSCommonsI::_version[]="v5.00";
 #endif
-uinteger AMSCommonsI::_build=631;
+uinteger AMSCommonsI::_build=641;
 uinteger AMSCommonsI::_buildtime=0;
 float AMSCommonsI::_mips=1000;
 uinteger AMSCommonsI::_os=0;
