dd797803bbe9f52594f422e8e670bf221f5988f3
barao <barao>
2013-06-12 19:23:13 +0000

    Bug fix in LIP RICH code (richpatt_lip.F, richchg_lip.F)

--- CC/commonsi.C
+++ CC/commonsi.C
@@ -1,4 +1,4 @@
-//  $Id: commonsi.C,v 1.225 2013/05/10 15:18:38 barao Exp $
+//  $Id: commonsi.C,v 1.226 2013/06/12 19:23:13 barao Exp $
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/time.h>
@@ -26,7 +26,7 @@ char AMSCommonsI::_version[]="v4.00";
 #else
 char AMSCommonsI::_version[]="v5.00";
 #endif
-uinteger AMSCommonsI::_build=651;
+uinteger AMSCommonsI::_build=652;
 uinteger AMSCommonsI::_buildtime=0;
 float AMSCommonsI::_mips=1000;
 uinteger AMSCommonsI::_os=0;
