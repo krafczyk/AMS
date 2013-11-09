7d108f55d16c1d49a6a90a2c0da16b0fd639db6a
mduranti <mduranti>
2013-11-09 17:25:25 +0000

    updated load_all removing no more interesting versions

--- install/load.C
+++ install/load.C
@@ -1,3 +1,9 @@
 {
-gSystem->Load("$AMSWD/lib/$ARCH/ntuple_slc4.so");
+  TString lib("$AMSWD/lib/$ARCH/ntuple_slc4.so");
+  gSystem->ExpandPathName(lib);
+
+  //  printf("\n");  
+  printf("******* Loading lib %s\n", lib.Data());
+  //  printf("\n");  
+  gSystem->Load(lib.Data());
 }
--- install/loadPG.C
+++ install/loadPG.C
@@ -1,3 +1,9 @@
 {
-gSystem->Load("$AMSWD/lib/$ARCH/ntuple_slc4_PG.so");
+  TString lib("$AMSWD/lib/$ARCH/ntuple_slc4_PG.so");
+  gSystem->ExpandPathName(lib);
+
+  //  printf("\n");
+  printf("******* Loading lib %s\n", lib.Data());
+  //  printf("\n");
+  gSystem->Load(lib.Data());
 }
