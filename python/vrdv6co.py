4af2d8e472acd00eb258d6fd8daabbb7e251b674
ams <ams>
2013-12-02 06:53:05 +0000

    *** empty log message ***

--- python/vrdv6co.py
+++ python/vrdv6co.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python2.4
-#  $Id: vrdv6co.py,v 1.1 2013/03/08 15:36:58 bshan Exp $
+#  $Id: vrdv6co.py,v 1.2 2013/12/02 06:53:05 ams Exp $
 
 import sys, os
 #sys.path.insert(0,os.environ['AMSDataDir'] + '/DataManagement/python/lib')
-sys.path.insert(0,os.environ['Offline'] + '/vdev/python/lib')
+sys.path.insert(0,'/var/www/cgi-bin/mon/lib')
 sys.path.insert(0,'./lib')
 import RemoteClient
 
@@ -12,10 +12,12 @@ v=1
 i=0
 d=0
 b=0
+castorcopy=0
 run2p=0
 h=0
 u=0
 mt=0
+force=0
 #u =1 for reverse unchecked runs to tobererun & deleting ntuples
 
 for x in sys.argv:
@@ -23,7 +25,10 @@ for x in sys.argv:
     elif x == "-i": i=1
     elif x == "-v": v=1
     elif x == "-d": d=1
+    elif x == "-dd": d=2
     elif x == "-u": u=1
+    elif x == "-castorcopy": castorcopy=1
+    elif x == "-force": force=1
     elif x == "-mt": mt=1
     elif x[0:2] == "-b":
         b=1
@@ -36,6 +41,6 @@ if(d==0):
 html= RemoteClient.RemoteClient()
 html.ConnectDB(1)
 if(html.ServerConnect(2)):
-    html.ValidateRuns(run2p,i,v,d,h,b,u,mt,1,0,0,1)
+    html.ValidateRuns(run2p,i,v,d,h,b,u,mt,1,force,0,1,castorcopy)
 
 
