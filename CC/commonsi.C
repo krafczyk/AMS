10c0e7085dd237804c2ff467af3e0771b415b975
choutko <choutko>
2013-12-04 16:32:59 +0000

    *** empty log message ***

--- CC/commonsi.C
+++ CC/commonsi.C
@@ -1,4 +1,4 @@
-//  $Id: commonsi.C,v 1.235.2.5 2013/11/29 16:15:39 choutko Exp $
+//  $Id: commonsi.C,v 1.235.2.6 2013/12/04 16:32:59 choutko Exp $
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/time.h>
@@ -26,7 +26,7 @@ char AMSCommonsI::_version[]="v4.00";
 #else
 char AMSCommonsI::_version[]="v5.01";
 #endif
-uinteger AMSCommonsI::_build=707;
+uinteger AMSCommonsI::_build=708;
 uinteger AMSCommonsI::_buildtime=0;
 float AMSCommonsI::_mips=1000;
 uinteger AMSCommonsI::_os=0;
