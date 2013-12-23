23903be1f6cfec276a9a5d9a44b597ea850bdcf8
bshan <bshan>
2013-12-23 15:47:14 +0000

    parse data journals

---
+++ python/parsejf.data.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python2.4
+
+import sys, os
+sys.path.insert(0,os.environ['Offline'] + '/vdev/python/lib')
+import RemoteClient
+
+v=1
+i=0
+d=1
+h=0
+mt=1
+castoronly=1
+force=0
+s=0
+m=0
+
+for x in sys.argv:
+    if x == "-h": h=1
+    elif x == "-i": i=1
+    elif x == "-v": v=1
+    elif x == "-d": d=1
+    elif x == "-castoronly": castoronly=1
+    elif x == "-mt": mt=1
+    elif x == "-s": s=1
+    elif x == "-m": m=1
+html= RemoteClient.RemoteClient()
+html.ConnectDB(1)
+if(html.ServerConnect()):
+    html.parseJournalFiles(d,i,v,h,s,m,mt,castoronly)
