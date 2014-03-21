afb238fe4ada0a81b3caa3aca4256ed042020411
choutko <choutko>
2014-03-21 15:20:44 +0000

    *** empty log message ***

--- DPMJET/src/G4DPMJET2_5CrossSection.cc
+++ DPMJET/src/G4DPMJET2_5CrossSection.cc
@@ -82,7 +82,11 @@ G4DPMJET2_5CrossSection::G4DPMJET2_5CrossSection ():
 // is applicable to hydrogen targets.  However, the cross-section will be
 // set to zero.
 //
+#if G4VERSION_NUMBER  > 945
   ATmin = 2;
+#else
+ATmin = 1;
+#endif
 // ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 //
 }
@@ -258,7 +262,9 @@ G4double G4DPMJET2_5CrossSection::GetIsoZACrossSection
     sigma = sigma * millibarn;
     double p[3]={0.48639E-01,0.41653E-04,0.77188E-03};
     double corr=p[0]+p[1]*AT+p[2]*AT*AT;
+#if G4VERSION_NUMBER  > 945
     sigma/=(1+corr);
+#endif
     if (verboseLevel >= 2) {
       G4cout <<"***************************************************************"
              <<G4endl;
