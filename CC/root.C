c64db3d55936436377fd1e083155860dc1f1b390
qyan <qyan>
2014-04-19 10:29:39 +0000

    TOF: implement new TOF time measurement calibration for new tuning MC

--- CC/root.C
+++ CC/root.C
@@ -2557,6 +2557,12 @@ bool AMSEventR::ReadHeader(int entry){
       if(TRFITFFKEY.magtemp && Version()>=800 ) MagField::GetPtr()->UpdateMagTemp(UTime());
     }
 #endif
+
+///--Load TOFDBc
+    if(AMSEventR::Head()->nMCEventgC() && Version()>=800){ //MC Event
+       TofTAlignPar *TofTAlign=TofTAlignPar::GetHead();
+       TofTAlign->TDVName="TofAlign2MC";//20140419 MC Calibration
+    } 
 #ifdef _PGTRACK_
     // Fix trdefaultfit
     if(Version()>700 && Version()<714){
