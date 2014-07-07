3cf3afc24ebdf067036f11753ffdc1fb9c3ebdc8
choutko <choutko>
2014-07-07 12:27:43 +0000

    change build no to 900 in preparation to B900 release

--- CC/commonsi.C
+++ CC/commonsi.C
@@ -26,7 +26,7 @@ char AMSCommonsI::_version[]="v4.00";
 #else
 char AMSCommonsI::_version[]="v5.01";
 #endif
-uinteger AMSCommonsI::_build=1000;
+uinteger AMSCommonsI::_build=900;
 uinteger AMSCommonsI::_buildtime=0;
 float AMSCommonsI::_mips=1000;
 uinteger AMSCommonsI::_os=0;
