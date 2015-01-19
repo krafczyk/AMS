895a814c5e3a9a5b60d60d2eb95954b20357f2be
incaglim <incaglim>
2015-01-19 03:16:23 +0000

    Slight modification to mipTB (16.45->16.4)

--- CC/job.C
+++ CC/job.C
@@ -1183,7 +1183,7 @@ void AMSJob::_reecaldata(){
   ECREFFKEY.Tgsl=0.25; // (125) -> Global slope for Temp correction (%/deg)
   //end of global Temp correction
 
-  ECREFFKEY.mipTB=16.45;  // (126) ADC value used to equalize mips at Test Beam
+  ECREFFKEY.mipTB=16.4;  // (126) ADC value used to equalize mips at Test Beam
 
   ECREFFKEY.mipTB2ISS=0.98;  // (127) Factor to convert TB mip (400 GeV protons) to ISS mip (E>5 GeV)
   FFKEY("ECRE",(float*)&ECREFFKEY,sizeof(ECREFFKEY_DEF)/sizeof(integer),"MIXED");
