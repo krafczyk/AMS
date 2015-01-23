9b6b6926e1a7cfdc3a905b4a9975dbe1672ea367
choutko <choutko>
2015-01-23 16:42:05 +0000

    *** empty log message ***

--- install/build410.sh
+++ install/build410.sh
@@ -1,8 +1,14 @@
 #!/bin/tcsh
-#source /afs/cern.ch/sw/lcg/external/gcc/4.8.1/x86_64-slc6/setup.csh  /afs/cern.ch/sw/lcg/external
+unsetenv DEBUGFLAG 
+setenv VERBOSE 1
+
 source /Offline/vdev/install/amsvar
 source $ROOTSYS/../root-v5-34-9-icc64.14-slc6/amsvar
 source /Offline/vdev/install/g4i10.1
+source /afs/cern.ch/sw/IntelSoftware/linux/all-setup.csh 
+source /afs/cern.ch/sw/IntelSoftware/linux/x86_64/xe2015/composerxe/bin/compilervars.csh   intel64
+setenv INTELDIR /afs/cern.ch/sw/IntelSoftware/linux/x86_64
+setenv INTELVER xe2015/composer_xe_2015.1.133
 setenv PGTRACK 1
 setenv AMSWD  ../g410
 setenv G4AMS 1
@@ -13,6 +19,8 @@ gmake producer6
 source /Offline/vdev/install/amsvar
 source $ROOTSYS/../root-v5-34-9-gcc64/amsvar
 source /Offline/vdev/install/g4i10.1
+source /afs/cern.ch/sw/lcg/external/gcc/4.8.1/x86_64-slc6/setup.csh  /afs/cern.ch/sw/lcg/external
+
 setenv PGTRACK 1
 setenv AMSWD  ../g410
 setenv G4AMS 1
