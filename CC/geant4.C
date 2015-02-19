412cd9a6d9c9420906bdc991ba101779fc1f4b67
choutko <choutko>
2015-02-19 15:58:19 +0000

    make simulation threads alert about barrier

--- CC/geant4.C
+++ CC/geant4.C
@@ -1150,13 +1150,13 @@ if(!Step)return;
 #endif
 trig=(trig+1)%freq;
 
-  if((trig==0 && AMSgObj::BookTimer.check("GEANTTRACKING")>AMSFFKEY.CpuLimit+g4_cpu_limit&& G4FFKEY.ApplyCPULimit) || (GCFLAG.IEORUN==1 || GCFLAG.IEOTRI==1)){
+  if((trig==0 && AMSgObj::BookTimer.check("GEANTTRACKING")>AMSFFKEY.CpuLimit+g4_cpu_limit&& G4FFKEY.ApplyCPULimit) || (GCFLAG.IEORUN==1 || GCFLAG.IEOTRI==1) || (AMSEvent::Barrier()>0 && AMSgObj::BookTimer.check("GEANTTRACKING")>AMSFFKEY.CpuLimit) ){
     freq=1;
     G4Track * Track = Step->GetTrack();
     GCTRAK.istop =1;
     Track->SetTrackStatus(fStopAndKill);
     AMSEvent::gethead()->seterror();
-    if(report)cerr<<"AMSG4EventAction::EndOfEventAction-E-CpuLimitExceeded Run Event "<<" "<<AMSEvent::gethead()->getrun()<<" "<<AMSEvent::gethead()->getid()<<" "<<AMSgObj::BookTimer.check("GEANTTRACKING")<<" "<<AMSFFKEY.CpuLimit+g4_cpu_limit<<" "<<g4_primary_momentum<<endl;
+    if(report)cerr<<"AMSG4EventAction::EndOfEventAction-E-CpuLimitExceeded Run Event "<<" "<<AMSEvent::gethead()->getrun()<<" "<<AMSEvent::gethead()->getid()<<" "<<AMSgObj::BookTimer.check("GEANTTRACKING")<<" "<<AMSFFKEY.CpuLimit+g4_cpu_limit<<" "<<g4_primary_momentum<<" "<<AMSEvent::Barrier()<<endl;
     report=false;
     AMSEvent::gethead()->SetEventSkipped(true);
     return;
