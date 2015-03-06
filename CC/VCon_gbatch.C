701f070219fb322b66fb73d00f94b46f8d86eec9
choutko <choutko>
2015-03-06 21:48:21 +0000

    *** empty log message ***

--- CC/VCon_gbatch.C
+++ CC/VCon_gbatch.C
@@ -2,7 +2,7 @@
 
 #include "event.h"
 
-VCon_gb::VCon_gb(AMSContainer * cc):VCon(){
+VCon_gb::VCon_gb(AMSContainer * cc):VCon(),leli(-1),lel(0){
   con=cc;
 }
 
@@ -10,9 +10,13 @@ VCon_gb::VCon_gb(AMSContainer * cc):VCon(){
 void  VCon_gb::removeEl(TrElem* prev, integer restore){
   if (!con)return;
   con->removeEl(dynamic_cast<AMSlink*> (prev),restore);
+  lel=0;
+  leli=-1;
 };
 
 VCon* VCon_gb::GetCont(const char * name){
+   lel=0;
+   leli=-1;
   AMSContainer *cc=AMSEvent::gethead()->getC(AMSID(name));
   if (cc){
     con=cc;
@@ -28,25 +32,42 @@ int VCon_gb::getnelem(){
 }
 
 void VCon_gb::eraseC(){
+   lel=0;
+   leli=-1;
   if(!con) return ;
   else return con->eraseC();
 }
 
 TrElem* VCon_gb::getelem(int ii){
   if(!con) return 0;
-  AMSlink* primo=con->gethead();
+  if(ii==leli+1 &&lel){
+   leli=ii;
+   lel=lel->next();
+    return dynamic_cast<TrElem*>(lel);
+  } 
+ else if(ii==leli &&lel)return dynamic_cast<TrElem*>(lel);
+AMSlink* primo=con->gethead();
   for (int jj=0;jj<ii;jj++)
     primo=primo->next();
+    leli=ii;
+    lel=primo;
   return dynamic_cast<TrElem*> (primo);
 }
 
 void VCon_gb::addnext(TrElem* aa){
   if(!con) return;
   con->addnext(dynamic_cast<AMSlink*> (aa));
+  
 }
 
 int  VCon_gb::getindex(TrElem* aa){
   if(!con) return 0;
+if(lel && lel==(AMSlink*)aa)return leli;
+else if(lel && lel->next() ==(AMSlink*)aa){
+lel=lel->next();
+leli++;
+return leli;
+}
   AMSlink* primo=con->gethead();
   for (int jj=0;jj<con->getnelem();jj++){
     if(primo==dynamic_cast<AMSlink*>(aa)) return jj;
@@ -56,6 +77,8 @@ int  VCon_gb::getindex(TrElem* aa){
 }
 
 void VCon_gb::exchangeEl(TrElem* el1, TrElem* el2) {
+   lel=0;
+   leli=-1;
   if(!con) return;
   con->exchangeEl(dynamic_cast<AMSlink*>(el1),dynamic_cast<AMSlink*>(el2));
 }
