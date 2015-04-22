f7f760ff41d42b69f765c7c2de069ad7ca2a634d
choutko <choutko>
2015-04-22 12:31:27 +0000

    *** empty log message ***

--- CC/job.C
+++ CC/job.C
@@ -323,7 +323,7 @@ void AMSJob::_siamsdata(){
   MISCFFKEY.ScanElemAbundance=0;    // (28) Activate a scan of element abudance along the track
 #ifdef G4MULTITHREADED
   MISCFFKEY.NoOrderedWrite=1;
-  MISCFFKEY.G4AllocatorSize=-8192000;
+  MISCFFKEY.G4AllocatorSize=-4000000;
 #else
   MISCFFKEY.NoOrderedWrite=0;
   MISCFFKEY.G4AllocatorSize=0;
